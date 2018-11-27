65398237100737873ed1cfb346696c892b6688e2
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-11-27 19:04:48 +0000

    Added Day 7 Part 2 solution.

--- src/main/buri/aoc/model/Program.java
+++ src/main/buri/aoc/model/Program.java
@@ -1,6 +1,7 @@
 package buri.aoc.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import java.util.Map;
 public class Program {
 	private String _name;
 	private int _weight;
+	private int _cachedTotalWeight = 0;
 	private List<String> _childNames;
 	private List<Program> _children;
 	
@@ -40,11 +42,53 @@ public class Program {
 	 * Recursively calculates the weight of this program and all children.
 	 */
 	public int getTotalWeight() {
-		int totalWeight = getWeight();
-		for (Program child : getChildren()) {
-			totalWeight += child.getTotalWeight();
+		if (_cachedTotalWeight == 0) {
+			int totalWeight = getWeight();
+			for (Program child : getChildren()) {
+				totalWeight += child.getTotalWeight();
+			}
+			_cachedTotalWeight = totalWeight;
 		}
-		return (totalWeight); 
+		return (_cachedTotalWeight); 
+	}
+	
+	/**
+	 * Searches for the 1 child with the wrong weight.
+	 */
+	public Program getImbalancedChild() {
+		Map<Integer, List<Program>> weightsToChildren = new HashMap<>();
+		for (Program child : getChildren()) {
+			if (weightsToChildren.get(child.getTotalWeight()) == null) {
+				weightsToChildren.put(child.getTotalWeight(), new ArrayList<>());
+			}
+			weightsToChildren.get(child.getTotalWeight()).add(child);
+		}
+		// No children, or all children have the same total weight.
+		if (getChildren().isEmpty() || weightsToChildren.size() == 1) {
+			return (null);
+		}
+		// Get the loner child. Assumption: The tree will have 3 or more children, so weight with only 1 child is the imbalanced one.
+		for (List<Program> children : weightsToChildren.values()) {
+			if (children.size() == 1) {
+				return (children.get(0));
+			}
+		}
+		throw new RuntimeException("Could not locate imbalanced child of program " + getName());
+	}
+	
+	/**
+	 * Prints the program tower.
+	 */
+	public String toString(int level) {
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < level; i++) {
+			buffer.append("\t");
+		}
+		buffer.append(getName()).append(" (").append(getWeight()).append(") (").append(getTotalWeight()).append(")\n");
+		for (Program child : getChildren()) {
+			buffer.append(child.toString(level + 1));
+		}
+		return (buffer.toString());
 	}
 	
 	/**
