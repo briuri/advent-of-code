e8e49ed0b77a236012d980e380b157550ebb6d96
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-11-30 01:49:42 +0000

    Added shell for Day 9 solution.

---
+++ src/2017/buri/aoc/y17/d09/Day09.java
@@ -0,0 +1,25 @@
+package buri.aoc.y17.d09;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day09 extends Puzzle {
+
+	/**
+	 * Input: A long string of groups and garbage.
+	 * Output: The string with last linebreak trimmed.
+	 */
+	public static String getInput(int fileIndex) {
+		return (readFile("2017/09", fileIndex).get(0));
+	}
+	
+	/**
+	 * Gets the total score for all groups.
+	 */
+	public static int getResult(Part part, String input) {
+		return (0);
+	}
+}
