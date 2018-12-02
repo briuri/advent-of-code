41c10171e79121418d6a51ec6cef9654ca0c5b99
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-02 05:36:59 +0000

    Added y18d2 solution.

--- trunk/src/2018/buri/aoc/y18/d02/Day02Test.java
+++ trunk/src/2018/buri/aoc/y18/d02/Day02Test.java
@@ -2,11 +2,10 @@ package buri.aoc.y18.d02;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-
-import buri.aoc.Part;
 
 /**
  * @author Brian Uri!
@@ -16,15 +15,34 @@ public class Day02Test {
 	@Test
 	public void testGetInput() {
 		List<String> content = Day02.getInput(0);
-		assertEquals(2074, content.size());
+		assertEquals(250, content.size());
 	}
 
 	/**
+	 * abcdef twos=false, threes=false
+	 * bababc twos=true, threes=true
+	 * abbcde twos=true, threes=false
+	 * abcccd twos=false, threes=true
+	 * aabcdd twos=true, threes=false
+	 * abcdee twos=true, threes=false
+	 * ababab twos=false, threes=true
 	 * 
+	 * Of these box IDs, 4 of them contain a letter which appears exactly twice, and 3 of them contain a letter
+	 * which appears exactly three times.
+	 * 
+	 * Multiplying these together produces a checksum of 4 * 3 = 12.
 	 */
 	@Test
 	public void testPart1Examples() {
-		assertEquals(1, Day02.getResult(Part.ONE, Day02.getInput(1)));
+		List<String> input = new ArrayList<>();
+		input.add("abcdef");
+		input.add("bababc");
+		input.add("abbcde");
+		input.add("abcccd");
+		input.add("aabcdd");
+		input.add("abcdee");
+		input.add("ababab");
+		assertEquals(12, Day02.getPart1Result(input));
 	}
 
 	/**
@@ -32,17 +50,39 @@ public class Day02Test {
 	 */
 	@Test
 	public void testPart1Puzzle() {
-		int result = Day02.getResult(Part.ONE, Day02.getInput(0));
+		int result = Day02.getPart1Result(Day02.getInput(0));
 		System.out.println("Day 2 Part 1\n\t" + result);
-		assertEquals(1, result);
+		assertEquals(5750, result);
 	}
 
 	/**
+	 * he boxes will have IDs which differ by exactly one character at the same position in both strings. For example,
+	 * given the following box IDs:
 	 * 
+	 * abcde
+	 * fghij
+	 * klmno
+	 * pqrst
+	 * fguij
+	 * axcye
+	 * wvxyz
+	 * 
+	 * The IDs fghij and fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+	 * What letters are common between the two correct box IDs?
+	 * 
+	 * In the example above, this is found by removing the differing character from either ID, producing fgij.
 	 */
 	@Test
 	public void testPart2Examples() {
-		assertEquals(1, Day02.getResult(Part.TWO, Day02.getInput(2)));
+		List<String> input = new ArrayList<>();
+		input.add("abcde");
+		input.add("fghij");
+		input.add("klmno");
+		input.add("pqrst");
+		input.add("fguij");
+		input.add("axcye");
+		input.add("wvxyz");
+		assertEquals("fgij", Day02.getPart2Result(input));
 	}
 
 	/**
@@ -50,8 +90,8 @@ public class Day02Test {
 	 */
 	@Test
 	public void testPart2Puzzle() {
-		int result = Day02.getResult(Part.TWO, Day02.getInput(0));
+		String result = Day02.getPart2Result(Day02.getInput(0));
 		System.out.println("Day 2 Part 2\n\t" + result);
-		assertEquals(1, result);
+		assertEquals("tzyvunogzariwkpcbdewmjhxi", result);
 	}
 }
