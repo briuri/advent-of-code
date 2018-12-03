26c92287ae18364f4f510cc78bc16831b8cfb7f1
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-03 14:17:21 +0000

    Added skeleton for y18d4.

---
+++ trunk/src/2018/buri/aoc/y18/d04/Day04Test.java
@@ -0,0 +1,57 @@
+package buri.aoc.y18.d04;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day04Test {
+
+	@Test
+	public void testGetInput() {
+		List<String> content = Day04.getInput(0);
+		assertEquals(2074, content.size());
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testPart1Examples() {
+		assertEquals("", Day04.getResult(Part.ONE, Day04.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		String result = Day04.getResult(Part.ONE, Day04.getInput(0));
+		System.out.println("Day 0 Part 1\n\t" + result);
+		assertEquals("", result);
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testPart2Examples() {
+		assertEquals("", Day04.getResult(Part.TWO, Day04.getInput(2)));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		String result = Day04.getResult(Part.TWO, Day04.getInput(0));
+		System.out.println("Day 0 Part 2\n\t" + result);
+		assertEquals("", result);
+	}
+}
