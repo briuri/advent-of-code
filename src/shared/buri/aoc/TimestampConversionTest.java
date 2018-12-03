c87903c270060ecce43334a522d5a3fb05a8ab18
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-03 05:58:12 +0000

    Updated conversion for 3 timestamps.

--- src/shared/buri/aoc/TimestampConversionTest.java
+++ src/shared/buri/aoc/TimestampConversionTest.java
@@ -7,10 +7,20 @@ import org.junit.Test;
 
 public class TimestampConversionTest {
 	
+	/**
+	 * Converts a UNIX timestamp into an amount of time after midnight.
+	 */
+	private static String convert(long timestamp) {
+		return (Date.from(Instant.ofEpochSecond(timestamp)).toString().substring(11, 19));
+	}
+	
 	@Test
 	public void testUnixTimestampConversion() {
-		long unixTimestamp = 1543727318;
-		String timestamp = Date.from(Instant.ofEpochSecond(unixTimestamp)).toString();
-		System.out.println(unixTimestamp + "=" + timestamp.substring(11, 19));
+		long unixTimestamp1 = 1543727318;
+		long unixTimestamp2 = 1543727590;
+		long unixTimestamp3 = 1543727679;
+		System.out.println(convert(unixTimestamp1));
+		System.out.println(convert(unixTimestamp2));
+		System.out.println(convert(unixTimestamp3));
 	}
 }
