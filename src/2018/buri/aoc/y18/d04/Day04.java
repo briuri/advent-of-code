1ec4e52ec55ba6320355f9476d32773fccb1e4d6
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-03 14:17:21 +0000

    Added skeleton for y18d4.

---
+++ src/2018/buri/aoc/y18/d04/Day04.java
@@ -0,0 +1,30 @@
+package buri.aoc.y18.d04;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day04 extends Puzzle {
+
+	/**
+	 * Input: 
+	 * Output: 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/04", fileIndex));
+	}
+	
+	/**
+	 * 
+	 */
+	public static String getResult(Part part, List<String> input) {
+		if (part == Part.ONE) {
+			return ("");
+		}
+		return ("");
+	}
+}
