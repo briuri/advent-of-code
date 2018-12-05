847ff39de4dc927b8ce6a54812b3b9bf518b4921
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-05 05:49:24 +0000

    y18d5 Solution

--- src/2018/buri/aoc/y18/d05/Day05Test.java
+++ src/2018/buri/aoc/y18/d05/Day05Test.java
@@ -2,8 +2,6 @@ package buri.aoc.y18.d05;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
-
 import org.junit.Test;
 
 import buri.aoc.Part;
@@ -15,16 +13,31 @@ public class Day05Test {
 
 	@Test
 	public void testGetInput() {
-		List<Data> input = Day05.getInput(0);
-		assertEquals(0, input.size());
+		String input = Day05.getInput(0);
+		assertEquals(50000, input.length());
 	}
 
 	/**
+	 * Now, consider a larger example, dabAcCaCBAcCcaDA:
 	 * 
+	 * dabAcCaCBAcCcaDA The first 'cC' is removed.
+	 * dabAaCBAcCcaDA This creates 'Aa', which is removed.
+	 * dabCBAcCcaDA Either 'cC' or 'Cc' are removed (the result is the same).
+	 * dabCBAcaDA No further actions can be taken.
+	 * 
+	 * After all possible reactions, the resulting polymer contains 10 units.
 	 */
 	@Test
 	public void testPart1Examples() {
-		assertEquals("", Day05.getResult(Part.ONE, Day05.getInput(1)));
+		assertEquals(10, Day05.getResult(Part.ONE, "dabAcCaCBAcCcaDA"));
+	}
+
+	/**
+	 * Real input failed because I wasn't backing index up far enough and it missed "pP".
+	 */
+	@Test
+	public void testEarlyIndexBugCase() {
+		assertEquals(1, Day05.getResult(Part.ONE, "pQqGgPz"));
 	}
 
 	/**
@@ -32,17 +45,25 @@ public class Day05Test {
 	 */
 	@Test
 	public void testPart1Puzzle() {
-		String result = Day05.getResult(Part.ONE, Day05.getInput(0));
-		System.out.println("Day 0 Part 1\n\t" + result);
-		assertEquals("", result);
+		int result = Day05.getResult(Part.ONE, Day05.getInput(0));
+		System.out.println("Day 5 Part 1\n\t" + result);
+		assertEquals(9686, result);
 	}
 
 	/**
+	 * For example, again using the polymer dabAcCaCBAcCcaDA from above:
 	 * 
+	 * Removing all A/a units produces dbcCCBcCcD. Fully reacting this polymer produces dbCBcD, which has length 6.
+	 * Removing all B/b units produces daAcCaCAcCcaDA. Fully reacting this polymer produces daCAcaDA, which has length
+	 * 8.
+	 * Removing all C/c units produces dabAaBAaDA. Fully reacting this polymer produces daDA, which has length 4.
+	 * Removing all D/d units produces abAcCaCBAcCcaA. Fully reacting this polymer produces abCBAc, which has length 6.
+	 * 
+	 * In this example, removing all C/c units was best, producing the answer 4.
 	 */
 	@Test
 	public void testPart2Examples() {
-		assertEquals("", Day05.getResult(Part.TWO, Day05.getInput(2)));
+		assertEquals(4, Day05.getResult(Part.TWO, "dabAcCaCBAcCcaDA"));
 	}
 
 	/**
@@ -50,8 +71,8 @@ public class Day05Test {
 	 */
 	@Test
 	public void testPart2Puzzle() {
-		String result = Day05.getResult(Part.TWO, Day05.getInput(0));
-		System.out.println("Day 0 Part 2\n\t" + result);
-		assertEquals("", result);
+		int result = Day05.getResult(Part.TWO, Day05.getInput(0));
+		System.out.println("Day 5 Part 2\n\t" + result);
+		assertEquals(5524, result);
 	}
 }
