d0dbbab0986e17a51a78dc1e267423fa153259ed
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-07 20:35:47 +0000

    y17d14 Skeleton

---
+++ trunk/src/2017/buri/aoc/y17/d14/Day14Test.java
@@ -0,0 +1,43 @@
+package buri.aoc.y17.d14;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day14Test {
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(8108, Day14.getResult(Part.ONE, "flqrgnkx"));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		int result = Day14.getResult(Part.ONE, "jxqlasbh");
+		System.out.println("Day 14 Part 1\n\t" + result);
+		assertEquals(0, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals("", Day14.getResult(Part.TWO, "flqrgnkx"));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		int result = Day14.getResult(Part.TWO, "jxqlasbh");
+		System.out.println("Day 14 Part 2\n\t" + result);
+		assertEquals("", result);
+	}
+}
