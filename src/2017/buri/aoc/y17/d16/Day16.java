d405abbc2cb3a8b3e2fed9226e47b3fc0de51b48
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-09 21:17:04 +0000

    WIP y17d16 Solution (part 2 still running).

---
+++ src/2017/buri/aoc/y17/d16/Day16.java
@@ -0,0 +1,40 @@
+package buri.aoc.y17.d16;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day16 extends Puzzle {
+
+	/**
+	 * Input: One line of dance commands
+	 * Output: A List of string commands
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (Arrays.asList(readFile("2017/16", fileIndex).get(0).split(",")));
+	}
+	
+	/**
+	 * Part 1:
+	 * In what order are the programs standing after their dance?
+	 */
+	public static String getResult(Part part, int numDancers, List<String> input) {
+		Dancers dancers = new Dancers(numDancers);
+		int iterations = (part == Part.ONE ? 1 : 1000000000);
+		for (int i = 0; i < iterations; i++) {
+			if (i % 100000 == 0) {
+				System.out.println(i + " " + new Date());
+			}
+			for (String move : input) {
+				dancers.perform(move);
+			}
+		}
+		return (dancers.toString());
+	}
+}
---
+++ src/2017/buri/aoc/y17/d16/Day16Test.java
@@ -0,0 +1,46 @@
+package buri.aoc.y17.d16;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day16Test {
+
+	@Test
+	public void testGetInput() {
+		List<String> input = Day16.getInput(0);
+		assertEquals(10000, input.size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals("baedc", Day16.getResult(Part.ONE, 5, Day16.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		String result = Day16.getResult(Part.ONE, 16, Day16.getInput(0));
+		System.out.println("Day 16 Part 1\n\t" + result);
+		assertEquals("jcobhadfnmpkglie", result);
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		String result = Day16.getResult(Part.TWO, 16, Day16.getInput(0));
+		System.out.println("Day 16 Part 2\n\t" + result);
+		assertEquals("", result);
+	}
+}
