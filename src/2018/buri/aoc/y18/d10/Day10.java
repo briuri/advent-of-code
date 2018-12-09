b6d05e5b71c072fced4ab2413dcb1b5d5dc27f5b
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-09 14:15:51 +0000

    y18d10 Skeleton

---
+++ src/2018/buri/aoc/y18/d10/Day10.java
@@ -0,0 +1,33 @@
+package buri.aoc.y18.d10;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day10 extends Puzzle {
+
+	/**
+	 * Input: 
+	 * Output: 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/10", fileIndex));
+	}
+	
+	/**
+	 * 
+	 */
+	public static int getResult(Part part, List<String> input) {
+		for (String data : input) {
+			
+		}
+		if (part == Part.ONE) {
+			return (0);
+		}
+		return (0);
+	}
+}
