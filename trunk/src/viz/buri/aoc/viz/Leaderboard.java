ca7d070000b37e28cfb273d4d32313c4f05d8889
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-11 15:15:39 +0000

    viz: Added titles and global links for all events.

--- trunk/src/viz/buri/aoc/viz/Leaderboard.java
+++ trunk/src/viz/buri/aoc/viz/Leaderboard.java
@@ -8,7 +8,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,56 +16,31 @@ import org.junit.Test;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Leaderboard {
-
-	private static final String CURRENT_EVENT = "2018";
 	private static final int TOTAL_PUZZLES = 25;
-	
-	private static final Map<Integer, Metadata> METADATA = new HashMap<>();
-	static {
-		METADATA.put(1, new Metadata("Chronal Calibration", 1));
-		METADATA.put(2, new Metadata("Inventory Management System", 0));
-		METADATA.put(3, new Metadata("No Matter How You Slice It", 0));
-		METADATA.put(4, new Metadata("Repose Record", 1));
-		METADATA.put(5, new Metadata("Alchemical Reduction", 0));
-		METADATA.put(6, new Metadata("Chronal Coordinates", 1));
-		METADATA.put(7, new Metadata("The Sum of Its Parts", 0));
-		METADATA.put(8, new Metadata("Memory Maneuver", 0));
-		METADATA.put(9, new Metadata("Marble Mania", 1));
-		METADATA.put(10, new Metadata("The Stars Align", 0));
-		METADATA.put(11, new Metadata("Chronal Charge", 1));
-		METADATA.put(12, new Metadata("", 0));
-		METADATA.put(13, new Metadata("", 0));
-		METADATA.put(14, new Metadata("", 0));
-		METADATA.put(15, new Metadata("", 0));
-		METADATA.put(16, new Metadata("", 0));
-		METADATA.put(17, new Metadata("", 0));
-		METADATA.put(18, new Metadata("", 0));
-		METADATA.put(19, new Metadata("", 0));
-		METADATA.put(20, new Metadata("", 0));
-		METADATA.put(21, new Metadata("", 0));
-		METADATA.put(22, new Metadata("", 0));
-		METADATA.put(23, new Metadata("", 0));
-		METADATA.put(24, new Metadata("", 0));
-		METADATA.put(25, new Metadata("", 0));
-	}
 	
 	@Test
 	public void visualizeLeaderboard() {
-		visualizeEvent("105906.json");
-		visualizeEvent("2017.json");
-		visualizeEvent("2016.json");
+		visualizeEvent("2018");
+		visualizeEvent("2017");
+		visualizeEvent("2016");
 	}
+
 	
-	private static void visualizeEvent(String filename) {
+	private static void visualizeEvent(String event) {
+		final String currentEvent = "2018";
+		final String pageTitle = "Novetta Advent of Code - Top 10 Solve Times";
+		final List<Metadata> metadata = readMetadata(event);
+		
+		// Read leaderboard.
 		Map<String, Object> members = null;
-		String event = null;
 		String lastModified = null;
-		// Read JSON
 		try {
 			ObjectMapper mapper = new ObjectMapper();
-			File file = new File("data/viz/" + filename);
+			File file = new File("data/viz/" + event + ".json");
 			lastModified = Date.from(Instant.ofEpochMilli(file.lastModified())).toString();
 			JsonNode json = mapper.readTree(file);
 			event = json.get("event").asText();
@@ -99,12 +73,11 @@ public class Leaderboard {
 			}
 		}
 
-		// Show the top finishes on each day.
-		String title = "Novetta Advent of Code - Top 10 Solve Times";
+		// Generate the HTML page.		
 		StringBuffer buffer = new StringBuffer();
 		
 		// Head
-		buffer.append("<html>\n<head><title>").append(title).append(" (").append(event).append(")").append("</title>\n");
+		buffer.append("<html>\n<head><title>").append(pageTitle).append(" (").append(event).append(")").append("</title>\n");
 		buffer.append("<style>\n");
 		buffer.append("\tbody { background-color: #0f0f23; color: #cccccc; font-family: monospace; font-size: 11pt; }\n");
 		buffer.append("\t.tiny { font-size: 9pt; }\n");
@@ -112,13 +85,11 @@ public class Leaderboard {
 		buffer.append("\ta:hover { color: #99ff99; }\n");
 		buffer.append("\t.global { color: #ffff00; }\n");
 		buffer.append("</style>\n</head>\n\n<body>\n");
-		buffer.append("<h2>").append(title).append(" (").append(event).append(")").append("</h2>\n\n");
+		buffer.append("<h2>").append(pageTitle).append(" (").append(event).append(")").append("</h2>\n\n");
 		buffer.append("<p class=\"tiny\">JSON downloaded from ");
-		buffer.append("<a href=\"https://adventofcode.com/").append(event).append("/leaderboard/private/view/105906\">Novetta Leaderboard</a>");
-		buffer.append(" on <b>").append(lastModified).append("</b>.");
-		if (event.equals("2018")) {
-			buffer.append("<br /><sup class=\"global\">*</sup> denotes a time that made the daily Global Leaderboard.</p>");
-		}
+		buffer.append("<a href=\"https://adventofcode.com/").append(event).append("/leaderboard/private/view/105906\">");
+		buffer.append("Novetta Leaderboard</a> on <b>").append(lastModified).append("</b>.");
+		buffer.append("<br /><sup class=\"global\">*</sup> Top 100 on the daily Global Leaderboard</p>");
 		buffer.append("</p>\n\n");
 		
 		// Nav Bar
@@ -136,22 +107,17 @@ public class Leaderboard {
 				int day = i + 1;
 				Collections.sort(places);
 				buffer.append("\n<h3><a href=\"https://adventofcode.com/").append(event).append("/day/").append(day);
-				buffer.append("\">Day ").append(day);
-				if (event.equals("2018")) {
-					buffer.append(": ").append(METADATA.get(day).getTitle());
-				}				
-				buffer.append("</a></h3>\n<ol>\n");
+				buffer.append("\">").append(metadata.get(i).getTitle()).append("</a></h3>\n");
+				buffer.append("<ol>\n");
 				int numPlaces = Math.min(10, places.size());
 				for (int place = 0; place < numPlaces; place++) {
 					Record record = places.get(place);
 					buffer.append("\t<li>").append(record.getPrettyTime(day));
-					if (event.equals("2018")) {
-						if (place + 1 <= METADATA.get(day).getGlobalCount()) {
-							buffer.append("<sup class=\"global\">*</sup>");
-						}
-						else {
-							buffer.append("&nbsp;");
-						}
+					if (place + 1 <= metadata.get(i).getGlobalCount()) {
+						buffer.append("<sup class=\"global\">*</sup>");
+					}
+					else {
+						buffer.append("&nbsp;");
 					}
 					buffer.append(" - ").append(record.getName()).append("</li>\n");
 				}
@@ -160,14 +126,37 @@ public class Leaderboard {
 		}
 		buffer.append("\n<p><a href=\"#\">Jump to Top</a></p>");
 		buffer.append("</body>\n</html>");
-		
+
 		// Save to file.
 		try {
-			String outputFilename = (event.equals(CURRENT_EVENT) ? "index.html" : "index-" + event + ".html");
+			String outputFilename = (event.equals(currentEvent) ? "index.html" : "index-" + event + ".html");
 			Files.write(Paths.get("output/" + outputFilename), buffer.toString().getBytes());
 		}
 		catch (IOException e) {
 			throw new IllegalArgumentException("Invalid output file", e);
+		}
+	}
+	
+	
+	/**
+	 * Reads the metadata from the file.
+	 */
+	private static List<Metadata> readMetadata(String event) {
+		List<Metadata> metadata = new ArrayList<>();
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			File file = new File("data/viz/metadata.json");
+			JsonNode json = mapper.readTree(file);
+			ArrayNode metadataJson = (ArrayNode) json.get(event);
+			for (int i = 0; i < metadataJson.size(); i++) {
+				ObjectNode puzzleMetadata = (ObjectNode) metadataJson.get(i);
+				metadata.add(new Metadata(puzzleMetadata.get("title").asText(), puzzleMetadata.get(
+					"globalCount").asInt()));
+			}
+			return (metadata);
+		}
+		catch (IOException e) {
+			throw new IllegalArgumentException("Invalid metadata file.", e);
 		}
 	}
 }
