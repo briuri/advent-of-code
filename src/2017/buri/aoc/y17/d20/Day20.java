fea1bf459b77cfc2e8dd148ebdf269515d26c6a4
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-12 21:06:17 +0000

    y17d20 Solution.

---
+++ src/2017/buri/aoc/y17/d20/Day20.java
@@ -0,0 +1,79 @@
+package buri.aoc.y17.d20;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day20 extends Puzzle {
+
+	/**
+	 * Input: 9 comma-separated values, 3 position, 3 velocity, 3 acceleration
+	 * Output: List of Particles
+	 */
+	public static List<Particle> getInput(int fileIndex) {
+		List<Particle> list = new ArrayList<>();
+		for (String particle : readFile("2017/20", fileIndex)) {
+			list.add(new Particle(particle));
+		}
+		return (list);
+	}
+
+	/**
+	 * Part 1:
+	 * Which particle will stay closest to position <0,0,0> in the long term?
+	 */
+	public static int getResult(Part part, List<Particle> input) {
+		if (part == Part.ONE) {
+			return (getSlowestAccelerationIndex(input));
+		}
+		for (int i = 0; i < 100; i++) {
+			moveWithCollisions(input);
+		}
+		return (input.size());
+	}
+
+	/**
+	 * Returns the particle index with the slowest acceleration.
+	 */
+	private static int getSlowestAccelerationIndex(List<Particle> input) {
+		int index = -1;
+		long minAcceleration = Long.MAX_VALUE;
+		for (int i = 0; i < input.size(); i++) {
+			Particle particle = input.get(i);
+			long acceleration = particle.getAcceleration().getManhattanDistance();
+			if (acceleration < minAcceleration) {
+				index = i;
+				minAcceleration = acceleration;
+			}
+		}
+		return (index);
+	}
+
+	/**
+	 * Removes any particles with the same position.
+	 */
+	private static void moveWithCollisions(List<Particle> input) {
+		Map<String, List<Particle>> collisions = new HashMap<>();
+		for (Particle particle : input) {
+			String position = particle.move();
+			if (collisions.get(position) == null) {
+				collisions.put(position, new ArrayList<>());
+			}
+			collisions.get(position).add(particle);
+		}
+		for (String position : collisions.keySet()) {
+			if (collisions.get(position).size() > 1) {
+				for (Particle collided : collisions.get(position)) {
+					input.remove(collided);
+				}
+			}
+		}
+	}
+}
---
+++ src/2017/buri/aoc/y17/d20/Triple.java
@@ -0,0 +1,81 @@
+package buri.aoc.y17.d20;
+
+/**
+ * Data class for a triple
+ * 
+ * @author Brian Uri!
+ */
+public class Triple {
+
+	private long _x;
+	private long _y;
+	private long _z;
+	
+	/**
+	 * Constructor
+	 */
+	public Triple(long x, long y, long z) {
+		_x = x;
+		_y = y;
+		_z = z;
+	}
+
+	
+	/**
+	 * Returns the Manhattan distance to 0,0,0.
+	 */
+	public long getManhattanDistance() {
+		return (Math.abs(getX()) + Math.abs(getY()) + Math.abs(getZ()));
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(getX()).append(",");
+		buffer.append(getY()).append(",");
+		buffer.append(getZ());
+		return (buffer.toString());
+	}
+	
+	/**
+	 * Accessor for the x
+	 */
+	public long getX() {
+		return _x;
+	}
+
+	/**
+	 * Accessor for the x
+	 */
+	public void setX(long x) {
+		_x = x;
+	}
+
+	/**
+	 * Accessor for the y
+	 */
+	public long getY() {
+		return _y;
+	}
+
+	/**
+	 * Accessor for the y
+	 */
+	public void setY(long y) {
+		_y = y;
+	}
+
+	/**
+	 * Accessor for the z
+	 */
+	public long getZ() {
+		return _z;
+	}
+
+	/**
+	 * Accessor for the z
+	 */
+	public void setZ(long z) {
+		_z = z;
+	}
+}
