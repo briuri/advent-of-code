166506aad422aafc5edfbd6994817176d6f1bbaa
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-12 06:18:18 +0000

    y18d12 Solution.

--- trunk/src/2018/buri/aoc/y18/d12/Day12.java
+++ trunk/src/2018/buri/aoc/y18/d12/Day12.java
@@ -11,23 +11,35 @@ import buri.aoc.Puzzle;
 public class Day12 extends Puzzle {
 
 	/**
-	 * Input: 
-	 * Output: 
+	 * Input: First line is initial state, remaining lines are patterns for growth.
+	 * Output: List of raw strings.
 	 */
 	public static List<String> getInput(int fileIndex) {
 		return (readFile("2018/12", fileIndex));
 	}
 	
 	/**
+	 * Part 1:
+	 * After 20 generations, what is the sum of the numbers of all pots which contain a plant?
 	 * 
+	 * Part 2:
+	 * After fifty billion (50000000000) generations, what is the sum of the numbers of all pots which contain a plant? 
 	 */
-	public static String getResult(Part part, List<String> input) {
-		for (String data : input) {
-			
-		}
+	public static long getResult(Part part, List<String> input) {
+		Pots pots = new Pots(input);
 		if (part == Part.ONE) {
-			return ("");
+			for (int i = 0; i < 20; i++) {
+				pots.grow();
+			}
+			return (pots.getGrowthSum());
 		}
-		return ("");
+		
+		// Part TWO		
+		// Visual inspection: After 125 iterations, sums are just 109 greater than the last.
+		for (int i = 0; i < 125; i++) {
+			pots.grow();
+		}
+		long sum = pots.getGrowthSum() + (109 * (50000000000L - pots.getGeneration()));
+		return (sum);
 	}
 }
