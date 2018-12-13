d3ef27792519164fd361fbe9f84194a62b1d2dc5
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-13 15:37:15 +0000

    y17d22 Solution

---
+++ src/2017/buri/aoc/y17/d22/Day22.java
@@ -0,0 +1,34 @@
+package buri.aoc.y17.d22;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day22 extends Puzzle {
+
+	/**
+	 * Input: Graph of the computing cluster
+	 * Output: The grid as an object
+	 */
+	public static Cluster getInput(int fileIndex) {
+		return (new Cluster(readFile("2017/22", fileIndex)));
+	}
+
+	/**
+	 * Part 1:
+	 * Given your actual map, after 10000 bursts of activity, how many bursts cause a node to become infected? (Do not
+	 * count nodes that begin infected.)
+	 * 
+	 * Part 2:
+	 * Given your actual map, after 10000000 bursts of activity, how many bursts cause a node to become infected? (Do
+	 * not count nodes that begin infected.)
+	 */
+	public static int getResult(Part part, Cluster input, int bursts) {
+		for (int i = 1; i <= bursts; i++) {
+			input.move(part);
+		}
+		return (input.getInfections());
+	}
+}
---
+++ src/2017/buri/aoc/y17/d22/Virus.java
@@ -0,0 +1,78 @@
+package buri.aoc.y17.d22;
+
+/**
+ * Data class for the virus carrier.
+ * 
+ * @author Brian Uri!
+ */
+public class Virus {
+	private Position _position;
+	private Direction _direction;
+
+	/**
+	 * Constructor
+	 */
+	public Virus(Position position) {
+		_position = position;
+		setDirection(Direction.UP);
+	}
+
+	/**
+	 * Part 1:
+	 * If the current node is infected, it turns to its right. Otherwise, it turns to its left. (Turning is done
+	 * in-place; the current node does not change.)
+	 * 
+	 * Part 2:
+	 * If it is clean, it turns left.
+	 * If it is weakened, it does not turn, and will continue moving in the same direction.
+	 * If it is infected, it turns right.
+	 * If it is flagged, it reverses direction, and will go back the way it came.
+	 */
+	public void turn(long node) {
+		if (node == Cluster.CLEAN) {
+			setDirection(getDirection().turnLeft());
+		}
+		else if (node == Cluster.WEAK) {
+			// Do nothing.
+		}
+		else if (node == Cluster.FLAGGED) {
+			setDirection(getDirection().reverse());
+		}
+		else if (node == Cluster.INFECTED) {
+			setDirection(getDirection().turnRight());
+		}
+	}
+
+	/**
+	 * The virus carrier moves forward one node in the direction it is facing.
+	 */
+	public void move() {
+		getPosition().move(getDirection());
+	}
+
+	@Override
+	public String toString() {
+		return (getPosition() + " " + getDirection());
+	}
+
+	/**
+	 * Accessor for the position
+	 */
+	public Position getPosition() {
+		return _position;
+	}
+
+	/**
+	 * Accessor for the direction
+	 */
+	public Direction getDirection() {
+		return _direction;
+	}
+
+	/**
+	 * Accessor for the direction
+	 */
+	private void setDirection(Direction direction) {
+		_direction = direction;
+	}
+}
