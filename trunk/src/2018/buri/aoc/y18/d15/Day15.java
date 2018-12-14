719cebd4a3ba2ac0fda5e669e6baca01d054c4b6
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-14 20:22:43 +0000

    y18d15 Skeleton.

---
+++ trunk/src/2018/buri/aoc/y18/d15/Day15.java
@@ -0,0 +1,31 @@
+package buri.aoc.y18.d15;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day15 extends Puzzle {
+
+	/**
+	 * Input: 
+	 * Output: 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/15", fileIndex));
+	}
+	
+	/**
+	 * 
+	 */
+	public static String getResult(Part part, List<String> input) {
+
+		if (part == Part.ONE) {
+			return ("");
+		}
+		return ("");
+	}
+}
