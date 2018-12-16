3a139dacc40ecc338ed5cfebafd368237a3c5ff3
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-16 06:24:31 +0000

    y18d16 Solution.

--- src/2018/buri/aoc/y18/d16/Day16.java
+++ src/2018/buri/aoc/y18/d16/Day16.java
@@ -11,21 +11,49 @@ import buri.aoc.Puzzle;
 public class Day16 extends Puzzle {
 
 	/**
-	 * Input: 
-	 * Output: 
+	 * For ease of processing, I split the raw input file into the data for Part 1 (16-0, 16-1) and Part 2 (16-2).
+	 * 
+	 * Part 1:
+	 * Input: Three lines per condition to test
+	 * Output: List of Strings
+	 * 
+	 * Part 2:
+	 * Input: One instruction per line.
+	 * Output: List of Strings
 	 */
 	public static List<String> getInput(int fileIndex) {
 		return (readFile("2018/16", fileIndex));
 	}
-	
-	/**
-	 * 
-	 */
-	public static String getResult(Part part, List<String> input) {
 
+	/**
+	 * Part 1:
+	 * Ignoring the opcode numbers, how many samples in your puzzle input behave like three or more opcodes?
+	 * 
+	 * Part 2:
+	 * What value is contained in register 0 after executing the test program?
+	 */
+	public static int getResult(Part part, List<String> input) {
 		if (part == Part.ONE) {
-			return ("");
+			int count = 0;
+			for (int i = 0; i < input.size(); i = i + 4) {
+				String before = input.get(i);
+				String[] code = input.get(i + 1).split(" ");
+				String after = input.get(i + 2);
+				Registers initial = new Registers(before.substring(before.indexOf('[') + 1, before.indexOf(']')));
+				Registers expected = new Registers(after.substring(after.indexOf('[') + 1, after.indexOf(']')));
+				int possibleMatches = initial.tryCode(code, expected);
+				if (possibleMatches >= 3) {
+					count++;
+				}
+			}
+			return (count);
 		}
-		return ("");
+
+		// Part TWO
+		Registers registers = new Registers();
+		for (int i = 0; i < input.size(); i++) {
+			registers.runCode(input.get(i).split(" "));
+		}
+		return (registers.get(Registers.REGISTER, 0));
 	}
 }
