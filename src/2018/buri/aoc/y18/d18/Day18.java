5fd8800c34ca083c94ebeecf40c770aff682fbdf
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-17 18:18:15 +0000

    y18d18 Skeleton

---
+++ src/2018/buri/aoc/y18/d18/Day18.java
@@ -0,0 +1,31 @@
+package buri.aoc.y18.d18;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day18 extends Puzzle {
+
+	/**
+	 * Input: 
+	 * Output: 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/18", fileIndex));
+	}
+	
+	/**
+	 * 
+	 */
+	public static String getResult(Part part, List<String> input) {
+
+		if (part == Part.ONE) {
+			return ("");
+		}
+		return ("");
+	}
+}
