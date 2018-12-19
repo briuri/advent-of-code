10f01208b7c3d7487ece2edadf9cb3533df7763b
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-19 20:24:26 +0000

    y18d20 Skeleton.

---
+++ src/2018/buri/aoc/y18/d20/Day20.java
@@ -0,0 +1,35 @@
+package buri.aoc.y18.d20;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * Day 20: TITLE
+ * 
+ * @author Brian Uri!
+ */
+public class Day20 extends Puzzle {
+
+	/**
+	 * Returns the input file HOW 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/20", fileIndex));
+	}
+	
+	/**
+	 * Part 1:
+	 * QUESTION
+	 * 
+	 * Part 2:
+	 * QUESTION
+	 */
+	public static int getResult(Part part, List<String> input) {
+		if (part == Part.ONE) {
+			return (0);
+		}
+		return (0);
+	}
+}
