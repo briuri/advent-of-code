961cdd8446a04a198455c44cafe47e45826b1f0e
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-20 16:26:22 +0000

    y18d21-d18d25 Skeletons.

---
+++ trunk/src/2018/buri/aoc/y18/d22/Day22.java
@@ -0,0 +1,35 @@
+package buri.aoc.y18.d22;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * Day 22: TITLE
+ * 
+ * @author Brian Uri!
+ */
+public class Day22 extends Puzzle {
+
+	/**
+	 * Returns the input file HOW 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/22", fileIndex));
+	}
+	
+	/**
+	 * Part 1:
+	 * QUESTION
+	 * 
+	 * Part 2:
+	 * QUESTION
+	 */
+	public static int getResult(Part part, List<String> input) {
+		if (part == Part.ONE) {
+			return (0);
+		}
+		return (0);
+	}
+}
---
+++ trunk/src/2018/buri/aoc/y18/d23/Day23.java
@@ -0,0 +1,35 @@
+package buri.aoc.y18.d23;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * Day 23: TITLE
+ * 
+ * @author Brian Uri!
+ */
+public class Day23 extends Puzzle {
+
+	/**
+	 * Returns the input file HOW 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2018/23", fileIndex));
+	}
+	
+	/**
+	 * Part 1:
+	 * QUESTION
+	 * 
+	 * Part 2:
+	 * QUESTION
+	 */
+	public static int getResult(Part part, List<String> input) {
+		if (part == Part.ONE) {
+			return (0);
+		}
+		return (0);
+	}
+}
