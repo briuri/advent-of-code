e104e729dd778363a768875c6e9bb927f4d50197
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-22 21:39:06 +0000

    y16d7 Solution.

---
+++ trunk/src/2016/buri/aoc/y16/d07/Day07Test.java
@@ -0,0 +1,51 @@
+package buri.aoc.y16.d07;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day07Test {
+
+	@Test
+	public void testGetInput() {
+		List<String> input = Day07.getInput(0);
+		assertEquals(2000, input.size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(2, Day07.getResult(Part.ONE, Day07.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		int result = Day07.getResult(Part.ONE, Day07.getInput(0));
+		System.out.println("Day 7 Part 1\n\t" + result);
+		assertEquals(110, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals(3, Day07.getResult(Part.TWO, Day07.getInput(2)));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		int result = Day07.getResult(Part.TWO, Day07.getInput(0));
+		System.out.println("Day 7 Part 2\n\t" + result);
+		assertEquals(242, result);
+	}
+}
