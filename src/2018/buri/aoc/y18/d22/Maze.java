83e9cbacb814a85a255f1a411c30e2516cfa48b4
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-22 13:20:56 +0000

    y18d22 Refactoring.

--- src/2018/buri/aoc/y18/d22/Maze.java
+++ src/2018/buri/aoc/y18/d22/Maze.java
@@ -2,6 +2,7 @@ package buri.aoc.y18.d22;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -17,20 +18,20 @@ import buri.aoc.data.grid.CharGrid;
  */
 public class Maze extends CharGrid {
 	private int _depth;
-	private Pair _target;
+	private Position _target;
 	private int[][] _erosionLevels;
 
 	private static final char ROCKY = '.';
 	private static final char WET = '=';
 	private static final char NARROW = '|';
 
-	private static final Pair MOUTH_POSITION = new Pair(0, 0);
-
-	// Use the same character as the terrain where the equipment is forbidden to simplify swap checks.
+	// Use the same character as the terrain where the item is forbidden to simplify swap checks.
 	private static final char NONE = ROCKY;
 	private static final char TORCH = WET;
 	private static final char GEAR = NARROW;
-	private static final char[] ALL_EQUIPMENT = new char[] { NONE, TORCH, GEAR };
+	private static final char[] ALL_ITEMS = new char[] { NONE, TORCH, GEAR };
+
+	private static final Position MOUTH_POSITION = new Position(0, 0, TORCH);
 
 	/**
 	 * Constructor
@@ -38,7 +39,7 @@ public class Maze extends CharGrid {
 	public Maze(int depth, Pair target) {
 		super(Math.max(target.getX(), target.getY()) + 10);
 		_depth = depth;
-		_target = target;
+		_target = new Position(target.getX(), target.getY(), TORCH);
 		_erosionLevels = new int[getSize()][getSize()];
 		for (int y = 0; y < getSize(); y++) {
 			for (int x = 0; x < getSize(); x++) {
@@ -114,93 +115,96 @@ public class Maze extends CharGrid {
 	/**
 	 * You start at MOUTH_POSITION with TORCH. The fastest route might involve entering regions beyond the X or Y
 	 * coordinate of the target. You can move (COST=1) to an adjacent region (up, down, left, or right; never
-	 * diagonally) if your currently equipped tool allows you to enter that region. (COST = 1 min) You can change your
-	 * currently equipped tool (COST=7) if your new equipment would be valid for your current region.
+	 * diagonally) if your currently equipped tool allows you to enter that region. (COST=1) You can change your
+	 * currently equipped tool (COST=7) if your new item would be valid for your current region.
 	 * 
 	 * You need TORCH to see target. Target is always in ROCK so if you arrive without TORCH, add (COST=7) to switch.
 	 */
 	public int getMinutes() {
-		// Do a Djikstra search for the path. Sort nodes based on time to reach them so far.
+		// Do a Djikstra search for the path. Sort nodes based on cost/time to reach them so far.
+		// Only revisit unique positions (triple of x, y, and items) when it's a loer cost than the last time we
+		// visited.
 		Queue<Node> frontier = new PriorityQueue<>();
-		frontier.add(new Node(MOUTH_POSITION, 0, TORCH));
-		Map<String, Integer> fastestTimes = new HashMap<>();
+		frontier.add(new Node(MOUTH_POSITION, 0));
+		Map<String, Integer> visitedNodes = new HashMap<>();
 		while (true) {
 			Node node = frontier.poll();
-			// System.out.println(node);
+
 			// Successfully found the target while wielding TORCH
-			if (node.getPair().equals(getTarget()) && node.getEquipment() == TORCH) {
+			if (node.getPosition().equals(getTarget())) {
 				return (node.getCostSoFar());
 			}
 
-			// Evaluate moving to any allowable cells while keeping our current equipment.
-			for (Pair neighbor : getTraversableNeighbors(node.getPair())) {
-				if (!requiresSwap(node.getEquipment(), neighbor)) {
-					String timeKey = createKey(neighbor, node.getEquipment());
-					Integer fastestTime = fastestTimes.get(timeKey);
-					int newTime = node.getCostSoFar() + 1;
-
-					// We found a faster way to go than the previous time we visited neighbor with this equipment.
-					if (fastestTime == null || newTime < fastestTime) {
-						frontier.add(new Node(neighbor, newTime, node.getEquipment()));
-						fastestTimes.put(timeKey, newTime);
-					}
-				}
+			// Evaluate moving to any allowable cells while keeping our current item.
+			for (Position next : getTraversableNeighbors(node.getPosition())) {
+				evaluateNext(frontier, visitedNodes, next, node.getCostSoFar() + 1);
 			}
 
-			// Evaluate staying here and swapping equipment.
-			for (char equipment : ALL_EQUIPMENT) {
-				if (requiresSwap(equipment, node.getPair()) && equipment != node.getEquipment()) {
-					String timeKey = createKey(node.getPair(), equipment);
-					Integer fastestTime = fastestTimes.get(timeKey);
-					int newTime = node.getCostSoFar() + 7;
-
-					// We found a faster way to go than the previous time we visited neighbor with this equipment.
-					if (fastestTime == null || newTime < fastestTime) {
-						frontier.add(new Node(node.getPair(), newTime, equipment));
-						fastestTimes.put(timeKey, newTime);
-					}
+			// Evaluate staying here and swapping items. (Always exactly 1 potential swap)
+			for (char item : ALL_ITEMS) {
+				if (item != node.getPosition().getItem() && isItemAllowed(item, node.getPosition())) {
+					Position next = new Position(node.getPosition().getX(), node.getPosition().getY(), item);
+					evaluateNext(frontier, visitedNodes, next, node.getCostSoFar() + 7);
 				}
 			}
 		}
 	}
 
 	/**
-	 * Creates a unique key based on a pair and equipment.
+	 * Helper method for Djikstra search that compares the time we reach the next node to the fastest time on record.
 	 */
-	private String createKey(Pair pair, int equipment) {
-		return (pair.toString() + "-" + equipment);
+	private void evaluateNext(Queue<Node> frontier, Map<String, Integer> visitedNodes, Position next, int nextTime) {
+		Integer fastestTime = visitedNodes.get(next.toString());
+		if (fastestTime == null || nextTime < fastestTime) {
+			frontier.add(new Node(next, nextTime));
+			visitedNodes.put(next.toString(), nextTime);
+		}
 	}
 
 	/**
-	 * Returns traversable cells adjacent to some position.
+	 * Returns traversable cells adjacent to some position, taking equipped item into account.
 	 */
-	private List<Pair> getTraversableNeighbors(Pair center) {
-		List<Pair> neighbors = new ArrayList<>();
+	private List<Position> getTraversableNeighbors(Position center) {
+		List<Position> neighbors = new ArrayList<>();
 		if (center.getY() >= 1) {
-			neighbors.add(new Pair(center.getX(), center.getY() - 1));
+			neighbors.add(new Position(center.getX(), center.getY() - 1, center.getItem()));
 		}
 		if (center.getX() >= 1) {
-			neighbors.add(new Pair(center.getX() - 1, center.getY()));
+			neighbors.add(new Position(center.getX() - 1, center.getY(), center.getItem()));
 		}
 		if (center.getX() < getSize() - 1) {
-			neighbors.add(new Pair(center.getX() + 1, center.getY()));
+			neighbors.add(new Position(center.getX() + 1, center.getY(), center.getItem()));
 		}
 		if (center.getY() < getSize() - 1) {
-			neighbors.add(new Pair(center.getX(), center.getY() + 1));
+			neighbors.add(new Position(center.getX(), center.getY() + 1, center.getItem()));
+		}
+
+		// Remove any with incompatible item / terrain matches.
+		for (Iterator<Position> iterator = neighbors.iterator(); iterator.hasNext();) {
+			Position position = iterator.next();
+			if (!isItemAllowed(position.getItem(), position)) {
+				iterator.remove();
+			}
 		}
 		return (neighbors);
 	}
 
 	/**
-	 * Returns true if the equipment must be swapped before entering next square. The character representation of each
-	 * piece of equipment matches the character of the terrain it cannot enter.
+	 * Returns true if the item is allowed on the terrain of the given position. The character representation of each
+	 * item matches the character of the terrain it cannot enter.
 	 * 
-	 * - TORCH (usable in ROCK, NARROW) (start with this equipped)
-	 * - GEAR (usable in ROCK, WET)
-	 * - NONE (usable in WET, NARROW)
+	 * Item allowed in terrain:
+	 * NONE (.) usable in WET (=) and NARROW (|)
+	 * TORCH (=) usable in ROCKY (.) and NARROW (|)
+	 * GEAR (|) usable in ROCKY (.) and WET (=)
+	 * 
+	 * Terrains and forbidden items:
+	 * ROCKY (.) forbids NONE (.)
+	 * WET (=) forbids TORCH (=)
+	 * NARROW (|) forbids GEAR (|)
 	 */
-	private boolean requiresSwap(char equipment, Pair next) {
-		return (equipment == get(next));
+	private boolean isItemAllowed(char item, Position position) {
+		return (item != get(position));
 	}
 
 	/**
@@ -220,7 +224,7 @@ public class Maze extends CharGrid {
 	/**
 	 * Accessor for the erosionLevels
 	 */
-	public int[][] getErosionLevels() {
+	private int[][] getErosionLevels() {
 		return _erosionLevels;
 	}
 }
