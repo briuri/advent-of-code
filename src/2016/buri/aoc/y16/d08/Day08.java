84e17af2e671ceb8b15c1b6f9d760b7a3889ae67
Brian Uri <13141618+briuri@users.noreply.github.com>
2018-12-23 21:17:06 +0000

    y16d8 Solution.

---
+++ src/2016/buri/aoc/y16/d08/Day08.java
@@ -0,0 +1,33 @@
+package buri.aoc.y16.d08;
+
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+
+/**
+ * Day 8: Two-Factor Authentication
+ * 
+ * @author Brian Uri!
+ */
+public class Day08 extends Puzzle {
+
+	/**
+	 * Returns the input file unmodified. 
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile("2016/08", fileIndex));
+	}
+	
+	/**
+	 * Part 1:
+	 * How many pixels should be lit?
+	 * 
+	 * Part 2:
+	 * After you swipe your card, what code is the screen trying to display?
+	 */
+	public static int getResult(Part part, List<String> input) {
+		Screen screen = new Screen();
+		return (screen.run(input));
+	}
+}
