0757271242ca27b5938fd83152ac0dc90bec01d5
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-23 09:13:30 +0000

    y18d23 Solution.

---
+++ trunk/src/2018/buri/aoc/y18/d23/Bot.java
@@ -0,0 +1,49 @@
+package buri.aoc.y18.d23;
+
+import buri.aoc.data.Triple;
+
+/**
+ * Data class for a nanobot (position and radius)
+ * 
+ * @author Brian Uri!
+ */
+public class Bot {
+
+	private Triple _position;
+	private long _radius;
+
+	/**
+	 * Constructor
+	 */
+	public Bot(String input) {
+		String[] tokens = input.split(",");
+		_position = new Triple(Long.valueOf(tokens[0]), Long.valueOf(tokens[1]), Long.valueOf(tokens[2]));
+		_radius = Long.valueOf(tokens[3]);
+	}
+
+	@Override
+	public String toString() {
+		return "[" + getPosition().toString() + " r=" + getRadius() + "]";
+	}
+	
+	/**
+	 * Returns true if some position is in range of this bot's signal.
+	 */
+	public boolean inRange(Triple position) {
+		return (getPosition().getManhattanDistance(position) <= getRadius());
+	}
+	
+	/**
+	 * Accessor for the position
+	 */
+	public Triple getPosition() {
+		return _position;
+	}
+
+	/**
+	 * Accessor for the radius
+	 */
+	public long getRadius() {
+		return _radius;
+	}
+}
--- trunk/src/2018/buri/aoc/y18/d23/Day23.java
+++ trunk/src/2018/buri/aoc/y18/d23/Day23.java
@@ -1,35 +1,155 @@
 package buri.aoc.y18.d23;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import buri.aoc.Part;
 import buri.aoc.Puzzle;
+import buri.aoc.data.Triple;
 
 /**
- * Day 23: TITLE
+ * Day 23: Experimental Emergency Teleportation
  * 
  * @author Brian Uri!
  */
 public class Day23 extends Puzzle {
 
 	/**
-	 * Returns the input file HOW 
+	 * Returns the input file as a list of nanobots.
 	 */
-	public static List<String> getInput(int fileIndex) {
-		return (readFile("2018/23", fileIndex));
+	public static List<Bot> getInput(int fileIndex) {
+		List<Bot> list = new ArrayList<>();
+		for (String line : readFile("2018/23", fileIndex)) {
+			list.add(new Bot(line));
+		}
+		return (list);
 	}
-	
+
 	/**
 	 * Part 1:
-	 * QUESTION
+	 * How many nanobots are in range of its signals?
 	 * 
 	 * Part 2:
-	 * QUESTION
+	 * Find the coordinates that are in range of the largest number of nanobots. What is the shortest manhattan distance
+	 * between any of those points and 0,0,0?
 	 */
-	public static int getResult(Part part, List<String> input) {
+	public static long getResult(Part part, List<Bot> bots) {
 		if (part == Part.ONE) {
-			return (0);
+			return (getBotsInRangeOfStrongest(bots));
 		}
-		return (0);
+		return (getShortestDistance(bots));
+	}
+
+	/**
+	 * Calculates how many bots are in range of a specific one.
+	 */
+	public static long getBotsInRangeOfStrongest(List<Bot> bots) {
+		long radius = Integer.MIN_VALUE;
+		Bot strongest = null;
+		for (Bot bot : bots) {
+			if (bot.getRadius() > radius) {
+				radius = bot.getRadius();
+				strongest = bot;
+			}
+		}
+
+		long inRange = 0;
+		for (Bot bot : bots) {
+			if (strongest.inRange(bot.getPosition())) {
+				inRange++;
+			}
+		}
+		return (inRange);
+	}
+
+	/**
+	 * Find the coordinates that are in range of the largest number of nanobots. if there are multiple, choose the one
+	 * closest to origin.
+	 * 
+	 * Naive solution (too slow):
+	 * - Store all possible coordinates in range of each bot in a frequency map.
+	 * - Determine the frequency of positions that occurs most.
+	 * - Find all positions that had that frequency and get the one with the smallest MD to origin.
+	 * 
+	 * Optimized solution (still too slow):
+	 * - Only put the coordinates that are common between any 2 bots in the frequency map.
+	 * - Determine the frequency of positions that occurs most.
+	 * - Find all positions that had that frequency and get the one with the smallest MD to origin.
+	 * 
+	 * Final solution:
+	 * - Get the bounds of all bot positions.
+	 * - Break volume into chunks and take sample readings of the maxBotsInRange at different positions.
+	 * - For the position with the most bots in range, resize the bounds and try again.
+	 * - Keep trying until the position converges to a maxBots (916) and the returned MD no longer changes.
+	 */
+	public static long getShortestDistance(List<Bot> bots) {
+		// Get the general area.
+		Triple min = new Triple(Long.MAX_VALUE, Long.MAX_VALUE, Long.MAX_VALUE);
+		Triple max = new Triple(Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE);
+		for (Bot bot : bots) {
+			min.setX(Math.min(min.getX(), bot.getPosition().getX()));
+			min.setY(Math.min(min.getY(), bot.getPosition().getY()));
+			min.setZ(Math.min(min.getZ(), bot.getPosition().getZ()));
+			max.setX(Math.max(max.getX(), bot.getPosition().getX()));
+			max.setY(Math.max(max.getY(), bot.getPosition().getY()));
+			max.setZ(Math.max(max.getZ(), bot.getPosition().getZ()));
+		}
+
+		// For my input, it took about 25 iterations before dx/dy/dz were all 1 and MD stabilized.
+		int i = 0;
+		long distance = 0;
+		while (i < 30) {
+			distance = sampleVolumeForMaxBots(bots, min, max);
+			i++;
+		}
+		return (distance);
+	}
+
+	/**
+	 * Samples points within a volume to see how many bots are in range. Finds the position with the most bots, then
+	 * resizes min / max around it so this method can be called again. Returns the current smallest MD.
+	 */
+	private static long sampleVolumeForMaxBots(List<Bot> bots, Triple min, Triple max) {
+		long chunk = 4;
+		long dx = Math.max(1, (max.getX() - min.getX()) / chunk);
+		long dy = Math.max(1, (max.getY() - min.getY()) / chunk);
+		long dz = Math.max(1, (max.getZ() - min.getZ()) / chunk);
+		Triple positionWithMaxBots = null;
+		int maxBotsInRange = 0;
+		for (long z = min.getZ(); z < max.getZ() + 1; z += dz) {
+			for (long y = min.getY(); y < max.getY() + 1; y += dy) {
+				for (long x = min.getX(); x < max.getX() + 1; x += dx) {
+
+					// Count up bots in range of this position.
+					Triple sampledTriple = new Triple(x, y, z);
+					int sampledMaxBots = 0;
+					for (Bot bot : bots) {
+						if (bot.inRange(sampledTriple)) {
+							sampledMaxBots++;
+						}
+					}
+
+					// We found a position with a higher number of bots than previously found.
+					if (positionWithMaxBots == null || sampledMaxBots > maxBotsInRange) {
+						positionWithMaxBots = sampledTriple;
+						maxBotsInRange = sampledMaxBots;
+					}
+					// When we have a tie, favor the one with the smaller distance.
+					else if (sampledMaxBots == maxBotsInRange
+						&& sampledTriple.getManhattanDistance() < positionWithMaxBots.getManhattanDistance()) {
+						positionWithMaxBots = sampledTriple;
+					}
+				}
+			}
+		}
+
+		// Adjust min / max for next iteration.
+		min.setX(positionWithMaxBots.getX() - dx);
+		min.setY(positionWithMaxBots.getY() - dy);
+		min.setZ(positionWithMaxBots.getZ() - dz);
+		max.setX(positionWithMaxBots.getX() + dx);
+		max.setY(positionWithMaxBots.getY() + dy);
+		max.setZ(positionWithMaxBots.getZ() + dz);
+		return (positionWithMaxBots.getManhattanDistance());
 	}
 }
