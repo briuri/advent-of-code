0e35cca4c3fc876495c99dbf39a75c666fc3e685
buri <buri@f70645ae-64a8-4c13-840d-cd591b2176f5>
2018-12-23 14:42:34 +0000

    Added one more failed approach.

--- trunk/src/2018/buri/aoc/y18/d23/Day23.java
+++ trunk/src/2018/buri/aoc/y18/d23/Day23.java
@@ -76,6 +76,9 @@ public class Day23 extends Puzzle {
 	 * - Determine the frequency of positions that occurs most.
 	 * - Find all positions that had that frequency and get the one with the smallest MD to origin.
 	 * 
+	 * Also failed:
+	 * - Try to calculate some sort of weighted center-of-mass (using radius to mock the weight) and search around there.
+	 * 
 	 * Final solution:
 	 * - Get the bounds of all bot positions.
 	 * - Break volume into chunks and take sample readings of the maxBotsInRange at different positions.
