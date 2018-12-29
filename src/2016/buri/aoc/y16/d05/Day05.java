7b54d8d00251fa3f7398a1b28a1e97eb8bf6902d
briuri <brian@urizone.net>
2018-12-29 17:20:46 -0500

    Refactored MD5 so it can be reused.

--- src/2016/buri/aoc/y16/d05/Day05.java
+++ src/2016/buri/aoc/y16/d05/Day05.java
@@ -1,10 +1,8 @@
 package buri.aoc.y16.d05;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import buri.aoc.Part;
 import buri.aoc.Puzzle;
+import buri.aoc.data.MD5Hash;
 
 /**
  * Day 5: How About a Nice Game of Chess?
@@ -26,48 +24,31 @@ public class Day05 extends Puzzle {
 	 * first result for each position and ignore invalid positions. What is the password?
 	 */
 	public static String getResult(Part part, String input) {
-		try {
-			MessageDigest digest = MessageDigest.getInstance("MD5");
-
-			int iteration = 0;
-			// Represent password as 8 blanks, completed when all are filled in.
-			StringBuffer password = new StringBuffer("        ");
-			while (password.toString().indexOf(" ") != -1) {
-				String hash = getHash(digest, input + iteration);
-				if (hash.startsWith("00000")) {
-					if (part == Part.ONE) {
-						// Fill in password in order.
-						password.setCharAt(password.indexOf(" "), hash.charAt(5));
-					}
-					else {
-						// Fill in specific characters in the password.
-						String possibleIndex = String.valueOf(hash.charAt(5));
-						if (possibleIndex.matches("[0-9]")) {
-							int index = Integer.valueOf(possibleIndex);
-							if (index < password.length() && password.charAt(index) == ' ') {
-								password.setCharAt(index, hash.charAt(6));
-							}
+		MD5Hash hasher = new MD5Hash();
+		
+		int iteration = 0;
+		// Represent password as 8 blanks, completed when all are filled in.
+		StringBuffer password = new StringBuffer("        ");
+		while (password.toString().indexOf(" ") != -1) {
+			String hash = hasher.getHash(input + iteration);
+			if (hash.startsWith("00000")) {
+				if (part == Part.ONE) {
+					// Fill in password in order.
+					password.setCharAt(password.indexOf(" "), hash.charAt(5));
+				}
+				else {
+					// Fill in specific characters in the password.
+					String possibleIndex = String.valueOf(hash.charAt(5));
+					if (possibleIndex.matches("[0-9]")) {
+						int index = Integer.valueOf(possibleIndex);
+						if (index < password.length() && password.charAt(index) == ' ') {
+							password.setCharAt(index, hash.charAt(6));
 						}
 					}
 				}
-				iteration++;
 			}
-			return (password.toString());
+			iteration++;
 		}
-		catch (NoSuchAlgorithmException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
-
-	/**
-	 * Computes an MD5 hash of a string.
-	 */
-	private static String getHash(MessageDigest digest, String input) {
-		StringBuffer buffer = new StringBuffer();
-		for (byte b : digest.digest(input.getBytes())) {
-			buffer.append(Character.forDigit((b >> 4) & 0xF, 16));
-			buffer.append(Character.forDigit((b & 0xF), 16));
-		}
-		return (buffer.toString());
+		return (password.toString());
 	}
 }
---
+++ src/shared/buri/aoc/data/MD5Hash.java
@@ -0,0 +1,45 @@
+package buri.aoc.data;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Utility class to compute an MD5 hash.
+ * 
+ * @author Brian Uri!
+ */
+public class MD5Hash {
+	private MessageDigest _digest;
+
+	/**
+	 * Constructor
+	 */
+	public MD5Hash() {
+		try {
+			_digest = MessageDigest.getInstance("MD5");
+
+		}
+		catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	/**
+	 * Computes an MD5 hash of a string.
+	 */
+	public String getHash(String input) {
+		StringBuffer buffer = new StringBuffer();
+		for (byte b : getDigest().digest(input.getBytes())) {
+			buffer.append(Character.forDigit((b >> 4) & 0xF, 16));
+			buffer.append(Character.forDigit((b & 0xF), 16));
+		}
+		return (buffer.toString());
+	}
+
+	/**
+	 * Accessor for the digest
+	 */
+	public MessageDigest getDigest() {
+		return _digest;
+	}
+}
