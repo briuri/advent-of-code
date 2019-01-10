319c92cc2c224c4f00036d72ec06e474b465a84c
briuri <brian@urizone.net>
2019-01-10 13:28:01 -0500

    y15d08 Solution

---
+++ src/2015/buri/aoc/y15/d08/Day08Test.java
@@ -0,0 +1,51 @@
+package buri.aoc.y15.d08;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day08Test {
+
+	@Test
+	public void testGetInput() {
+		List<String> input = Day08.getInput(0);
+		assertEquals(300, input.size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(12, Day08.getResult(Part.ONE, Day08.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		int result = Day08.getResult(Part.ONE, Day08.getInput(0));
+		System.out.println("Day 0 Part 1\n\t" + result);
+		assertEquals(1333, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals(19, Day08.getResult(Part.TWO, Day08.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		int result = Day08.getResult(Part.TWO, Day08.getInput(0));
+		System.out.println("Day 0 Part 2\n\t" + result);
+		assertEquals(2046, result);
+	}
+}
