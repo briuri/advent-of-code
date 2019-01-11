872a24dacaec5241c42b92e885d38297f106df05
briuri <brian@urizone.net>
2019-01-11 12:49:55 -0500

    Fixed test output.

--- src/2015/buri/aoc/y15/d12/Day12Test.java
+++ src/2015/buri/aoc/y15/d12/Day12Test.java
@@ -23,7 +23,7 @@ public class Day12Test {
 	@Test
 	public void testPart1Puzzle() {
 		int result = Day12.getResult(Part.ONE, Day12.getInput(0));
-		System.out.println("Day 0 Part 1\n\t" + result);
+		System.out.println("Day 12 Part 1\n\t" + result);
 		assertEquals(111754, result);
 	}
 
@@ -33,7 +33,7 @@ public class Day12Test {
 	@Test
 	public void testPart2Puzzle() {
 		int result = Day12.getResult(Part.TWO, Day12.getInput(0));
-		System.out.println("Day 0 Part 2\n\t" + result);
+		System.out.println("Day 12 Part 2\n\t" + result);
 		assertEquals(65402, result);
 	}
 }
