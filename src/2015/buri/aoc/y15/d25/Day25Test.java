fec0ad8c30798cfc49433d13201bd060ee65f993
briuri <brian@urizone.net>
2019-01-13 14:27:46 -0500

    y15d25 Solution

---
+++ src/2015/buri/aoc/y15/d25/Day25Test.java
@@ -0,0 +1,24 @@
+package buri.aoc.y15.d25;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+import buri.aoc.data.Pair;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day25Test {
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		long result = Day25.getResult(Part.ONE, new Pair(2947, 3029));
+		System.out.println("Day 25 Part 1\n\t" + result);
+		assertEquals(0, result);
+	}
+}
