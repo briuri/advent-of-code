bd1e61f18298eceaf2cc7ba7d755deb3871c755d
briuri <brian@urizone.net>
2019-01-14 10:54:15 -0500

    y15d24 Solution

---
+++ src/2015/buri/aoc/y15/d24/Day24Test.java
@@ -0,0 +1,51 @@
+package buri.aoc.y15.d24;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day24Test {
+
+	@Test
+	public void testGetInput() {
+		List<String> input = Day24.getInput(0);
+		assertEquals(29, input.size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(99, Day24.getResult(Part.ONE, Day24.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		long result = Day24.getResult(Part.ONE, Day24.getInput(0));
+		System.out.println("Day 24 Part 1\n\t" + result);
+		assertEquals(10723906903L, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals(44, Day24.getResult(Part.TWO, Day24.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		long result = Day24.getResult(Part.TWO, Day24.getInput(0));
+		System.out.println("Day 24 Part 2\n\t" + result);
+		assertEquals(74850409, result);
+	}
+}
