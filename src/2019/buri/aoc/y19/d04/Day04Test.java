644be9856694bc01a0645f388fa599a98e5d8f32
briuri <brian@urizone.net>
2019-12-07 23:21:07 -0500

    y19d4 Solution.

---
+++ src/2019/buri/aoc/y19/d04/Day04Test.java
@@ -0,0 +1,55 @@
+package buri.aoc.y19.d04;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day04Test {
+
+	@Test
+	public void testGetInput() {
+		List<Integer> input = Day04.getInput(0);
+		assertEquals(2, input.size());
+	}
+	
+	@Test
+	public void testPart1Examples() {
+		assertTrue(Day04.isValidPassword(Part.ONE, "111111"));
+		assertFalse(Day04.isValidPassword(Part.ONE, "223450"));
+		assertFalse(Day04.isValidPassword(Part.ONE, "123789"));
+	}
+	
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		int result = Day04.getResult(Part.ONE, Day04.getInput(0));
+		System.out.println("Day 04 Part 1\n\t" + result);
+		assertEquals(1929, result);
+	}
+	
+	@Test
+	public void testPart2Examples() {
+		assertTrue(Day04.isValidPassword(Part.TWO, "112233"));
+		assertFalse(Day04.isValidPassword(Part.TWO, "123444"));
+		assertTrue(Day04.isValidPassword(Part.TWO, "111122"));
+	}
+	
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		int result = Day04.getResult(Part.TWO, Day04.getInput(0));
+		System.out.println("Day 04 Part 1\n\t" + result);
+		assertEquals(1306, result);
+	}
+}
