ebc758f6d8745f04e9ed2055258531c035d9d20f
briuri <brian@urizone.net>
2019-12-10 14:26:59 -0500

    y19d9 Solution.

---
+++ src/2019/buri/aoc/y19/d09/Day09.java
@@ -0,0 +1,41 @@
+package buri.aoc.y19.d09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import buri.aoc.Part;
+import buri.aoc.Puzzle;
+import buri.aoc.data.intcode.Computer;
+
+/**
+ * Day 09: Sensor Boost
+ * 
+ * @author Brian Uri!
+ */
+public class Day09 extends Puzzle {
+
+	/**
+	 * Returns the input file as a list of longs 
+	 */
+	public static List<Long> getInput(int fileIndex) {
+		List<Long> list = new ArrayList<>();
+		for (String input : readFile("2019/09", fileIndex).get(0).split(",")) {
+			list.add(Long.valueOf(input));
+		}
+		return (list);
+	}
+	
+	/**
+	 * Part 1:
+	 * What BOOST keycode does it produce?
+	 * 
+	 * Part 2:
+	 * What are the coordinates of the distress signal?
+	 */
+	public static long getResult(Part part, List<Long> program) {
+		long input = part == Part.ONE ? 1 : 2;
+		Computer computer = new Computer(program, input);
+		computer.run(null);
+		return (computer.getLastOutput());
+	}
+}
---
+++ src/2019/buri/aoc/y19/d09/Day09Test.java
@@ -0,0 +1,51 @@
+package buri.aoc.y19.d09;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day09Test extends BaseTest {
+
+	@Test
+	public void testGetInput() {
+		List<Long> input = Day09.getInput(0);
+		assertEquals(973, input.size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(99, Day09.getResult(Part.ONE, Day09.getInput(1)));
+		assertEquals(1219070632396864L, Day09.getResult(Part.ONE, Day09.getInput(2)));
+		assertEquals(1125899906842624L, Day09.getResult(Part.ONE, Day09.getInput(3)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		long result = Day09.getResult(Part.ONE, Day09.getInput(0));
+		toClipboard(result);
+		System.out.println("Day 9 Part 1\n\t" + result);
+		assertEquals(2870072642L, result);
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		long result = Day09.getResult(Part.TWO, Day09.getInput(0));
+		toClipboard(result);
+		System.out.println("Day 9 Part 2\n\t" + result);
+		assertEquals(58534L, result);
+	}
+}
