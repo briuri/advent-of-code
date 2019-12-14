711f6f688410d0afe6f2d98cf3c216b5c52d7dee
briuri <brian@urizone.net>
2019-12-14 07:36:53 -0500

    y19d12 Solution.

---
+++ src/2019/buri/aoc/y19/d12/Day12Test.java
@@ -0,0 +1,56 @@
+package buri.aoc.y19.d12;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day12Test extends BaseTest {
+
+	@Test
+	public void testGetInput() {
+		List<String> input = Day12.getInput(0);
+		assertEquals(4, input.size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(179, Day12.getResult(Part.ONE, Day12.getInput(1), 10));
+		assertEquals(1940, Day12.getResult(Part.ONE, Day12.getInput(2), 100));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		long result = Day12.getResult(Part.ONE, Day12.getInput(0), 1000);
+		toClipboard(result);
+		System.out.println("Day 12 Part 1\n\t" + result);
+		assertEquals(8362, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals(2772, Day12.getResult(Part.TWO, Day12.getInput(1), 0));
+		assertEquals(4686774924L, Day12.getResult(Part.TWO, Day12.getInput(2), 0));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		long result = Day12.getResult(Part.TWO, Day12.getInput(0), 0);
+		toClipboard(result);
+		System.out.println("Day 12 Part 2\n\t" + result);
+		assertEquals(478373365921244L, result);
+	}
+}
---
+++ src/2019/buri/aoc/y19/d12/Moon.java
@@ -0,0 +1,122 @@
+package buri.aoc.y19.d12;
+
+import buri.aoc.data.Triple;
+
+/**
+ * Data model for one of Jupiter's moons
+ * 
+ * @author Brian Uri!
+ */
+public class Moon {
+	private Triple _position;
+	private Triple _velocity;
+
+	/**
+	 * Constructor
+	 * 
+	 * <x=-8, y=-10, z=0>
+	 */
+	public Moon(String input) {
+		String[] tokens = input.split(", ");
+		String x = tokens[0].split("=")[1];
+		String y = tokens[1].split("=")[1];
+		String z = tokens[2].split("=")[1].split(">")[0];
+		_position = new Triple(Long.valueOf(x), Long.valueOf(y), Long.valueOf(z));
+		_velocity = new Triple(0, 0, 0);
+	}
+
+	/**
+	 * Adjusts velocity based on the gravity of another moon.
+	 * 
+	 * To apply gravity, consider every pair of moons. On each axis (x, y, and z), the velocity of each
+	 * moon changes by exactly +1 or -1 to pull the moons together. For example, if Ganymede has an x
+	 * position of 3, and Callisto has a x position of 5, then Ganymede's x velocity changes by +1
+	 * (because 5 > 3) and Callisto's x velocity changes by -1 (because 3 < 5). However, if the
+	 * positions on a given axis are the same, the velocity on that axis does not change for that pair
+	 * of moons.
+	 */
+	public void applyGravity(Moon moon) {
+		int dx = calculateGravity(getPosition().getX(), moon.getPosition().getX());
+		int dy = calculateGravity(getPosition().getY(), moon.getPosition().getY());
+		int dz = calculateGravity(getPosition().getZ(), moon.getPosition().getZ());
+		getVelocity().setX(getVelocity().getX() + dx);
+		getVelocity().setY(getVelocity().getY() + dy);
+		getVelocity().setZ(getVelocity().getZ() + dz);
+	}
+
+	/**
+	 * On each axis (x, y, and z), the velocity of each moon changes by exactly +1 or -1 to pull the moons together. For
+	 * example, if Ganymede has an x position of 3, and Callisto has a x position of 5, then Ganymede's x velocity
+	 * changes by +1 (because 5 > 3) and Callisto's x velocity changes by -1 (because 3 < 5). However, if the
+	 * positions on a given axis are the same, the velocity on that axis does not change for that pair
+	 * of moons.
+	 */
+	private int calculateGravity(long posA, long posB) {
+		if (posA > posB) {
+			return (-1);
+		}
+		else if (posA < posB)  {
+			return (1);
+		}
+		return (0);
+	}
+
+	/**
+	 * Adjusts the position based on the current velocity.
+	 */
+	public void move() {
+		getPosition().setX(getPosition().getX() + getVelocity().getX());
+		getPosition().setY(getPosition().getY() + getVelocity().getY());
+		getPosition().setZ(getPosition().getZ() + getVelocity().getZ());
+	}
+
+	/**
+	 * Calculates the total energy of the moon.
+	 */
+	public long getEnergy() {
+		long pe = getPosition().getManhattanDistance();
+		long ke = getVelocity().getManhattanDistance();
+		return (pe * ke);
+	}
+	
+	/**
+	 * Returns a snapshot of the x-values.
+	 */
+	public String getXState() {
+		return (getPosition().getX() + "|" + getVelocity().getX());
+	}
+
+	/**
+	 * Returns a snapshot of the y-values.
+	 */
+	public String getYState() {
+		return (getPosition().getY() + "|" + getVelocity().getY());
+	}
+
+	/**
+	 * Returns a snapshot of the z-values.
+	 */
+	public String getZState() {
+		return (getPosition().getZ() + "|" + getVelocity().getZ());
+	}
+	
+	@Override
+	public String toString() {
+		return (getPosition() + "|" + getVelocity());
+	}
+	
+	/**
+	 * Accessor for the position
+	 */
+	public Triple getPosition() {
+		return _position;
+	}
+
+	/**
+	 * Accessor for the velocity
+	 */
+	public Triple getVelocity() {
+		return _velocity;
+	}
+
+}
