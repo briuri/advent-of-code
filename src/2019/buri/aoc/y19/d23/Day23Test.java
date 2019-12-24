e36824c33cfc7031af65cbb12960abe02b55a9ba
briuri <brian@urizone.net>
2019-12-24 16:52:24 -0500

    y19d23 Solution.

---
+++ src/2019/buri/aoc/y19/d23/Day23Test.java
@@ -0,0 +1,44 @@
+package buri.aoc.y19.d23;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day23Test extends BaseTest {
+
+	@Test
+	public void testGetInput() {
+		List<Long> input = Day23.getInput(0);
+		assertEquals(2273, input.size());
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		long result = Day23.getResult(Part.ONE, Day23.getInput(0));
+		toClipboard(result);
+		System.out.println("Day 23 Part 1\n\t" + result);
+		assertEquals(17541L, result);
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		long result = Day23.getResult(Part.TWO, Day23.getInput(0));
+		toClipboard(result);
+		System.out.println("Day 23 Part 2\n\t" + result);
+		assertEquals(12415, result);
+	}
+}
