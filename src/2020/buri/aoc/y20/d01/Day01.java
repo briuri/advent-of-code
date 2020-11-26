6d40f8d71cc87e6f481ab2b4e80473836daf2ae3
briuri <brian@urizone.net>
2020-11-26 14:36:07 -0500

    Set up for y20.

---
+++ src/2020/buri/aoc/y20/d01/Day01.java
@@ -0,0 +1,47 @@
+package buri.aoc.y20.d01;
+
+import java.util.List;
+
+import buri.aoc.BasePuzzle;
+import buri.aoc.Part;
+
+/**
+ * Day 01: TITLE
+ *
+ * @author Brian Uri!
+ */
+public class Day01 extends BasePuzzle {
+
+	/**
+	 * Returns the input file HOW
+	 */
+	public static List<String> getInput(int fileIndex) {
+		return (readFile(fileIndex));
+		// List<Data> list = new ArrayList<>();
+		// for (String input : readFile(fileIndex)) {
+		// list.add(new Data(input));
+		// }
+		// return (list);
+	}
+
+	/**
+	 * Part 1:
+	 * QUESTION
+	 *
+	 * Part 2:
+	 * QUESTION
+	 */
+	public static int getResult(Part part, List<String> input) {
+		if (part == Part.ONE) {
+			return (0);
+		}
+		return (0);
+	}
+
+	/**
+	 *
+	 */
+	protected static String doSomething() {
+		return ("");
+	}
+}
---
+++ src/2020/buri/aoc/y20/d01/Day01Test.java
@@ -0,0 +1,49 @@
+package buri.aoc.y20.d01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+
+/**
+ * @author Brian Uri!
+ */
+public class Day01Test extends BaseTest {
+
+	@Test
+	public void testGetInput() {
+		assertEquals(1, Day01.getInput(0).size());
+	}
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(0, Day01.getResult(Part.ONE, Day01.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		int result = Day01.getResult(Part.ONE, Day01.getInput(0));
+		toConsole(result);
+		assertEquals(0, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals(0, Day01.getResult(Part.TWO, Day01.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		int result = Day01.getResult(Part.TWO, Day01.getInput(0));
+		toConsole(result);
+		assertEquals(0, result);
+	}
+}
