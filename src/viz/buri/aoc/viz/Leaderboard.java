04a95bdaa31b65c825b4fb7de2b6a6668107392f
briuri <brian@urizone.net>
2020-12-01 12:02:30 -0500

    Refactored viz overflow leaderboard code.

--- src/viz/buri/aoc/viz/Leaderboard.java
+++ src/viz/buri/aoc/viz/Leaderboard.java
@@ -129,27 +129,23 @@ public class Leaderboard {
 			JsonNode json = mapper.readTree(file);
 			leaderboardJson = mapper.readValue(json.get("members").toString(),
 				new TypeReference<Map<String, Object>>() {});
-		}
-		catch (IOException e) {
-			throw new IllegalArgumentException("Invalid file.", e);
-		}
 
-		// Merge in overflow leaderboard, if available.
-		try {
-			File file = new File(JSON_FOLDER + year + "-1.json");
-			JsonNode json = mapper.readTree(file);
-			Map<String, Object> overflow = mapper.readValue(json.get("members").toString(),
-				new TypeReference<Map<String, Object>>() {});
-			for (String key : overflow.keySet()) {
-				if (!leaderboardJson.containsKey(key)) {
-					leaderboardJson.put(key, overflow.get(key));
+			// Merge in overflow leaderboard, if available.
+			File overflowFile = new File(JSON_FOLDER + year + "-1.json");
+			if (overflowFile.exists()) {
+				json = mapper.readTree(file);
+				Map<String, Object> overflow = mapper.readValue(json.get("members").toString(),
+					new TypeReference<Map<String, Object>>() {});
+				for (String key : overflow.keySet()) {
+					if (!leaderboardJson.containsKey(key)) {
+						leaderboardJson.put(key, overflow.get(key));
+					}
 				}
 			}
 		}
 		catch (IOException e) {
 			throw new IllegalArgumentException("Invalid file.", e);
 		}
-
 		return (leaderboardJson);
 	}
 
