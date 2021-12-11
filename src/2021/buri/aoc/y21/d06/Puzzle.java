4b808db6080e2e06170d723bca572aefaf69d7a6
briuri <brian@urizone.net>
2021-12-11 08:19:49 -0500

    y21d06 solution.

---
+++ src/2021/buri/aoc/y21/d06/Puzzle.java
@@ -0,0 +1,61 @@
+package buri.aoc.y21.d06;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import buri.aoc.BasePuzzle;
+import buri.aoc.Part;
+
+/**
+ * Day 06: Lanternfish
+ *
+ * @author Brian Uri!
+ */
+public class Puzzle extends BasePuzzle {
+
+	/**
+	 * Returns the input file as a list of integers.
+	 */
+	public static List<Integer> getInput(int fileIndex) {
+		String[] stringInts = readFile(fileIndex).get(0).split(",");
+		return (convertStringsToInts(Arrays.asList(stringInts)));
+	}
+
+	/**
+	 * Part 1:
+	 * How many lanternfish would there be after 80 days?
+	 *
+	 * Part 2:
+	 * How many lanternfish would there be after 256 days?
+	 */
+	public static long getResult(Part part, List<Integer> input) {
+		int days = (part == Part.ONE ? 80 : 256);
+		Map<Integer, Long> counts = new HashMap<>();
+		for (int fish : input) {
+			counts.put(fish, counts.getOrDefault(fish, 0L) + 1);
+		}
+
+		for (int day = 0; day < days; day++) {
+
+			// Decrease each fish timer by 1.
+			for (int timer = 0; timer <= 8; timer++) {
+				counts.put(timer - 1, counts.getOrDefault(timer, 0L));
+
+			}
+
+			// Reallocated expired fish timers.
+			long expired = counts.getOrDefault(-1, 0L);
+			counts.put(6, counts.getOrDefault(6, 0L) + expired);
+			counts.put(8, expired);
+			counts.remove(-1);
+		}
+
+		long sum = 0;
+		for (int fish = 0; fish <= 8; fish++) {
+			sum += counts.getOrDefault(fish, 0L);
+		}
+		return (sum);
+	}
+}
