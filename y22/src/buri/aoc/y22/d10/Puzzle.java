bb64e8f26c67a3f2b0970116dd588f18a872f8fa
buri <brian@urizone.net>
2022-12-13 23:49:23 -0500

    y22d10 solution.

---
+++ y22/src/buri/aoc/y22/d10/Puzzle.java
@@ -0,0 +1,84 @@
+package buri.aoc.y22.d10;
+
+import buri.aoc.BasePuzzle;
+import buri.aoc.Part;
+import buri.aoc.data.grid.CharGrid;
+import buri.aoc.data.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Day 10: Cathode-Ray Tube
+ *
+ * @author Brian Uri!
+ */
+public class Puzzle extends BasePuzzle {
+
+	private static final List<Integer> STRENGTH_CYCLES = new ArrayList<>();
+	static {
+		STRENGTH_CYCLES.add(20);
+		STRENGTH_CYCLES.add(60);
+		STRENGTH_CYCLES.add(100);
+		STRENGTH_CYCLES.add(140);
+		STRENGTH_CYCLES.add(180);
+		STRENGTH_CYCLES.add(220);
+	}
+
+	private static final int GRID_WIDTH = 40;
+	private static final int GRID_HEIGHT = 6;
+
+	/**
+	 * Part 1:
+	 * What is the sum of these six signal strengths?
+	 *
+	 * Part 2:
+	 * What eight capital letters appear on your CRT?
+	 */
+	public static String getResult(Part part, List<String> input) {
+		int xRegister = 1;
+		int cycle = 1;
+		int signalStrength = 0;
+		StringBuilder grid = new StringBuilder();
+
+		for (String line : input) {
+			String[] tokens = line.split(" ");
+			if (tokens[0].equals("noop")) {
+				draw(grid, cycle, xRegister);
+				cycle += 1;
+				signalStrength = signalStrength + checkStrength(cycle, xRegister);
+			}
+			else if (tokens[0].equals("addx")) {
+				draw(grid, cycle, xRegister);
+				cycle += 1;
+				signalStrength = signalStrength + checkStrength(cycle, xRegister);
+
+				draw(grid, cycle, xRegister);
+				cycle += 1;
+				xRegister += Integer.parseInt(tokens[1]);
+				signalStrength = signalStrength + checkStrength(cycle, xRegister);
+			}
+		}
+
+		// Covert into 40x6 grid after the fact.
+		for (int height = GRID_HEIGHT - 1; height >= 1; height--) {
+			grid.insert(height * GRID_WIDTH, '\n');
+		}
+		return (part == Part.ONE ? String.valueOf(signalStrength) : grid.toString());
+	}
+
+	/**
+	 * Calculates a signal strength at specific cycles.
+	 */
+	protected static int checkStrength(int cycle, int xRegister) {
+		return (STRENGTH_CYCLES.contains(cycle) ? cycle * xRegister : 0);
+	}
+
+	/**
+	 * Writes to the CRT grid if the cursor's x-position is inside the xRegister sprite.
+	 */
+	protected static void draw(StringBuilder grid, int cycle, int xRegister) {
+		int x = (cycle - 1) % GRID_WIDTH;
+		grid.append(xRegister - 1 <= x && x <= xRegister + 1 ? '■' : ' ');
+	}
+}
---
+++ y22/src/buri/aoc/y22/d10/PuzzleTest.java
@@ -0,0 +1,46 @@
+package buri.aoc.y22.d10;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Brian Uri!
+ */
+public class PuzzleTest extends BaseTest {
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals("13140", Puzzle.getResult(Part.ONE, Puzzle.getInput(1)));
+	}
+
+	/**
+	 * Solves the Part 1 puzzle.
+	 */
+	@Test
+	public void testPart1Puzzle() {
+		String result = Puzzle.getResult(Part.ONE, Puzzle.getInput(0));
+		toConsole(result);
+		assertEquals("13680", result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		String result = Puzzle.getResult(Part.TWO, Puzzle.getInput(1));
+		assertTrue(result.startsWith("■■  ■■  ■■  ■■  ■■  ■■  ■■  ■■  ■■  ■■  "));
+	}
+
+	/**
+	 * Solves the Part 2 puzzle.
+	 */
+	@Test
+	public void testPart2Puzzle() {
+		// Visual inspection: PZGPKPEB
+		String result = Puzzle.getResult(Part.TWO, Puzzle.getInput(0));
+		toConsole(result);
+		assertTrue(result.startsWith("■■■  ■■■■  ■■  ■■■  ■  ■ ■■■  ■■■■ ■■■  "));
+	}
+}
