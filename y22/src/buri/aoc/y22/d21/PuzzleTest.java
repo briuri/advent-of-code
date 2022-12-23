dbb86ae98f4161e28a1d6e7185efac7dfc0fc503
buri <brian@urizone.net>
2022-12-23 11:06:22 -0500

    y22d21 solution.

---
+++ y22/src/buri/aoc/y22/d21/PuzzleTest.java
@@ -0,0 +1,37 @@
+package buri.aoc.y22.d21;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Brian Uri!
+ */
+public class PuzzleTest extends BaseTest {
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(152L, Puzzle.getResult(Part.ONE, Puzzle.getInput(1)));
+	}
+
+	@Test
+	public void testPart1Puzzle() {
+		long result = Puzzle.getResult(Part.ONE, Puzzle.getInput(0));
+		toConsole(result);
+		assertEquals(299983725663456L, result);
+	}
+
+	@Test
+	public void testPart2Examples() {
+		assertEquals(301L, Puzzle.getResult(Part.TWO, Puzzle.getInput(1)));
+	}
+
+	@Test
+	public void testPart2Puzzle() {
+		long result = Puzzle.getResult(Part.TWO, Puzzle.getInput(0));
+		toConsole(result);
+		assertEquals(3093175982595L, result);
+	}
+}
