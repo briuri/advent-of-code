eddcc167575ce2f2c3b5fe48340b8893b9d43d04
buri <brian@urizone.net>
2022-12-29 14:08:14 -0500

    y22d24 solution.

---
+++ y22/src/buri/aoc/y22/d24/Blizzards.java
@@ -0,0 +1,107 @@
+package buri.aoc.y22.d24;
+
+import buri.aoc.data.Direction;
+import buri.aoc.data.tuple.Triple;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Data class for blizzard positions at different times.
+ */
+public class Blizzards {
+
+	private final int _totalMinutes;
+	private final int _upWall, _downWall, _leftWall, _rightWall;
+	private final Set<Triple<Integer>> _blizzards = new HashSet<>();
+
+	/**
+	 * Constructor
+	 */
+	public Blizzards(int totalMinutes, int upWall, int downWall, int leftWall, int rightWall) {
+		_totalMinutes = totalMinutes;
+		_upWall = upWall;
+		_downWall = downWall;
+		_leftWall = leftWall;
+		_rightWall = rightWall;
+	}
+
+	/**
+	 * Adds a blizzard to the collection and simulates its movement over time.
+	 */
+	public void addBlizzard(int x, int y, Direction direction) {
+		Triple<Integer> blizzard = new Triple<>(x, y, 0);
+		getBlizzards().add(blizzard);
+		for (int minute = 1; minute < getTotalMinutes(); minute++) {
+			blizzard = blizzard.copy();
+			if (direction == Direction.UP) {
+				blizzard.setY(blizzard.getY() - 1);
+				if (blizzard.getY() == getUpWall()) {
+					blizzard.setY(getDownWall() - 1);
+				}
+			}
+			if (direction == Direction.DOWN) {
+				blizzard.setY(blizzard.getY() + 1);
+				if (blizzard.getY() == getDownWall()) {
+					blizzard.setY(getUpWall() + 1);
+				}
+			}
+			if (direction == Direction.LEFT) {
+				blizzard.setX(blizzard.getX() - 1);
+				if (blizzard.getX() == getLeftWall()) {
+					blizzard.setX(getRightWall() - 1);
+				}
+			}
+			if (direction == Direction.RIGHT) {
+				blizzard.setX(blizzard.getX() + 1);
+				if (blizzard.getX() == getRightWall()) {
+					blizzard.setX(getLeftWall() + 1);
+				}
+			}
+			blizzard.setZ(blizzard.getZ() + 1);
+			getBlizzards().add(blizzard);
+		}
+	}
+
+	/**
+	 * Accessor for the total number of snapshots to simulate.
+	 */
+	public int getTotalMinutes() {
+		return _totalMinutes;
+	}
+
+	/**
+	 * Accessor for the y position of the upper wall.
+	 */
+	public int getUpWall() {
+		return _upWall;
+	}
+
+	/**
+	 * Accessor for the y position of the lower wall.
+	 */
+	public int getDownWall() {
+		return _downWall;
+	}
+
+	/**
+	 * Accessor for the x position of the left wall.
+	 */
+	public int getLeftWall() {
+		return _leftWall;
+	}
+
+	/**
+	 * Accessor for the x position of the right wall.
+	 */
+	public int getRightWall() {
+		return _rightWall;
+	}
+
+	/**
+	 * Accessor for the blizzard snapshots.
+	 */
+	public Set<Triple<Integer>> getBlizzards() {
+		return _blizzards;
+	}
+}
---
+++ y22/src/buri/aoc/y22/d24/Puzzle.java
@@ -0,0 +1,114 @@
+package buri.aoc.y22.d24;
+
+import buri.aoc.BasePuzzle;
+import buri.aoc.Part;
+import buri.aoc.data.Direction;
+import buri.aoc.data.tuple.Pair;
+import buri.aoc.data.tuple.Triple;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Day 24: Blizzard Basin
+ *
+ * @author Brian Uri!
+ */
+public class Puzzle extends BasePuzzle {
+
+	private static final int MAX_MINUTES = 900;
+
+	/**
+	 * Part 1:
+	 * What is the fewest number of minutes required to avoid the blizzards and reach the goal?
+	 *
+	 * Part 2:
+	 * What is the fewest number of minutes required to reach the goal, go back to the start, then reach the goal again?
+	 */
+	public static long getResult(Part part, List<String> input) {
+		int downWall = input.size() - 1;
+		int rightWall = input.get(0).length() - 1;
+		Blizzards blizzards = new Blizzards(MAX_MINUTES, 0, downWall, 0, rightWall);
+		for (int y = 0; y <= downWall; y++) {
+			for (int x = 0; x <= rightWall; x++) {
+				Direction direction = Direction.getDirectionFor(input.get(y).charAt(x));
+				if (direction != null) {
+					blizzards.addBlizzard(x, y, direction);
+				}
+			}
+		}
+
+		Triple<Integer> start = new Triple<>(1, 0, 0);
+		Pair<Integer> end = new Pair<>(rightWall - 1, downWall);
+		int fewest = getFewestMinutes(start, end, blizzards);
+		if (part == Part.TWO) {
+			// Back to start
+			start = new Triple<>(rightWall - 1, downWall, fewest);
+			end = new Pair<>(1, 0);
+			fewest += getFewestMinutes(start, end, blizzards);
+			// Back to end
+			start = new Triple<>(1, 0, fewest);
+			end = new Pair<>(rightWall - 1, downWall);
+			fewest += getFewestMinutes(start, end, blizzards);
+		}
+		return fewest;
+	}
+
+	/**
+	 * Calculates the number of minutes needed to get from one spot (at a time) to another.
+	 */
+	protected static int getFewestMinutes(Triple<Integer> start, Pair<Integer> end, Blizzards blizzards) {
+		List<Triple<Integer>> frontier = new ArrayList<>();
+		frontier.add(start);
+		while (!frontier.isEmpty()) {
+			Set<Triple<Integer>> newFrontier = new HashSet<>();
+			for (Triple<Integer> current : frontier) {
+				if (current.getZ() > MAX_MINUTES) {
+					break;
+				}
+				for (Triple<Integer> next : getNextSteps(current, start, end, blizzards)) {
+					newFrontier.add(next);
+					if (next.getX().equals(end.getX()) && next.getY().equals(end.getY())) {
+						return (next.getZ() - start.getZ());
+					}
+				}
+			}
+			frontier = new ArrayList<>(newFrontier);
+		}
+		throw new RuntimeException("Couldn't find the destination.");
+	}
+
+	protected static List<Triple<Integer>> getNextSteps(Triple<Integer> current, Triple<Integer> start,
+														Pair<Integer> end, Blizzards blizzards) {
+		boolean onStart = current.getX().equals(start.getX()) && current.getY().equals(start.getY());
+		List<Triple<Integer>> nextSteps = new ArrayList<>();
+		nextSteps.add(new Triple<>(current.getX(), current.getY(), current.getZ() + 1));
+		nextSteps.add(new Triple<>(current.getX(), current.getY() - 1, current.getZ() + 1));
+		nextSteps.add(new Triple<>(current.getX(), current.getY() + 1, current.getZ() + 1));
+		nextSteps.add(new Triple<>(current.getX() - 1, current.getY(), current.getZ() + 1));
+		nextSteps.add(new Triple<>(current.getX() + 1, current.getY(), current.getZ() + 1));
+
+		for (Iterator<Triple<Integer>> iterator = nextSteps.iterator(); iterator.hasNext(); ) {
+			Triple<Integer> position = iterator.next();
+			boolean nextIsStart = position.getX().equals(start.getX()) && position.getY().equals(start.getY());
+			// Special wall case: Allow start spot if we haven't left it yet.
+			if (onStart && nextIsStart) {
+				continue;
+			}
+			// Special wall case: Allow us to reach the end spot.
+			if (position.getX().equals(end.getX()) && position.getY().equals(end.getY())) {
+				continue;
+			}
+			// Remove out of bounds, walls, or blizzards
+			if (position.getX() <= blizzards.getLeftWall() || position.getX() >= blizzards.getRightWall()
+					|| position.getY() <= blizzards.getUpWall() || position.getY() >= blizzards.getDownWall()
+					|| blizzards.getBlizzards().contains(position)) {
+				iterator.remove();
+			}
+		}
+		return (nextSteps);
+	}
+}
