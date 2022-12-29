9c3035166fca4cfbd62362d4088bcf47783ac4dd
buri <brian@urizone.net>
2022-12-29 14:06:08 -0500

    y22d22 solution.

---
+++ y22/src/buri/aoc/y22/d22/Position.java
@@ -0,0 +1,87 @@
+package buri.aoc.y22.d22;
+
+import buri.aoc.data.Direction;
+import buri.aoc.data.tuple.Pair;
+
+import java.util.Objects;
+
+/**
+ * Data class for a point and a facing.
+ *
+ * @author Brian Uri!
+ */
+public class Position {
+
+	private Pair<Integer> _point;
+	private Direction _facing;
+
+	/**
+	 * Constructor
+	 */
+	public Position(int x, int y, Direction facing) {
+		this(new Pair<>(x, y), facing);
+	}
+
+	/**
+	 * Constructor
+	 */
+	public Position(Pair<Integer> point, Direction facing) {
+		_point = point;
+		_facing = facing;
+	}
+
+	/**
+	 * Moves in the current direction.
+	 */
+	public void move() {
+		getPoint().move(getFacing());
+	}
+
+	/**
+	 * Creates a deep copy.
+	 */
+	public Position copy() {
+		return new Position(getPoint().copy(), getFacing());
+	}
+
+	@Override
+	public String toString() {
+		return ("[" + getPoint() + " " + getFacing() + "]");
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Position position = (Position) o;
+		return Objects.equals(getPoint(), position.getPoint()) && getFacing() == position.getFacing();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getPoint(), getFacing());
+	}
+
+	/**
+	 * Accessor for the point
+	 */
+	public Pair<Integer> getPoint() {
+		return _point;
+	}
+
+	/**
+	 * Accessor for the direction facing
+	 */
+	public Direction getFacing() {
+		return _facing;
+	}
+
+	/**
+	 * Accessor for the direction facing
+	 */
+	public void setFacing(Direction facing) {
+		_facing = facing;
+	}
+}
---
+++ y22/src/buri/aoc/y22/d22/PuzzleTest.java
@@ -0,0 +1,38 @@
+package buri.aoc.y22.d22;
+
+import buri.aoc.BaseTest;
+import buri.aoc.Part;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Brian Uri!
+ */
+public class PuzzleTest extends BaseTest {
+
+	@Test
+	public void testPart1Examples() {
+		assertEquals(6032L, Puzzle.getResult(Part.ONE, Puzzle.getInput(1)));
+	}
+
+	@Test
+	public void testPart1Puzzle() {
+		long result = Puzzle.getResult(Part.ONE, Puzzle.getInput(0));
+		toConsole(result);
+		assertEquals(155060L, result);
+	}
+
+// My solution was hardcoded to the real input.
+//	@Test
+//	public void testPart2Examples() {
+//		assertEquals(5031L, Puzzle.getResult(Part.TWO, Puzzle.getInput(1)));
+//	}
+
+	@Test
+	public void testPart2Puzzle() {
+		long result = Puzzle.getResult(Part.TWO, Puzzle.getInput(0));
+		toConsole(result);
+		assertEquals(3479L, result);
+	}
+}
