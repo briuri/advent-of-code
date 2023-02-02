2b2851e0eb1a6ce2cea121ccd65d7f894e4ed3ab
buri <brian@urizone.net>
2023-02-02 12:55:46 -0500

    y16d04 solution.

---
+++ puzzles-kotlin/src/buri/aoc/y16/d04/Puzzle.kt
@@ -0,0 +1,76 @@
+package buri.aoc.y16.d04
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Part
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun(1514, 1)
+        assertRun(137896, 0, true)
+    }
+    @Test
+    fun runPart2() {
+        assertRun(501, 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): Number {
+        var realSum = 0
+        for (line in input) {
+            val frequencies = mutableMapOf<Char, Int>()
+            val name = line.substring(0, line.lastIndexOf("-"))
+            val id = line.substring(line.lastIndexOf("-") + 1, line.indexOf("[")).toInt()
+            val checksum = line.substring(line.indexOf("[") + 1, line.lastIndex)
+            for (value in name.filter { it.isLowerCase() }) {
+                frequencies.putIfAbsent(value, 0)
+                frequencies[value] = frequencies[value]!! + 1
+            }
+            var testChecksum = ""
+            for (count in frequencies.values.distinct().sorted().reversed()) {
+                for (key in frequencies.keys.sorted()) {
+                    if (frequencies[key] == count) {
+                        testChecksum += key
+                    }
+                }
+            }
+            if (testChecksum.substring(0, 5) == checksum) {
+                realSum += id
+                val unencrypted = rotate(name, id)
+                if (part == Part.TWO && unencrypted == "northpole object storage") {
+                    return id
+                }
+            }
+        }
+        return realSum
+    }
+
+    /**
+     * Decrypts the name using the sector ID
+     */
+    private fun rotate(name: String, id: Int): String {
+        val rotation = id % 26
+        var next = ""
+        for (i in name.indices) {
+            if (name[i] == '-') {
+                next += ' '
+            }
+            else {
+                var value = name[i] + rotation
+                if (value > 'z') {
+                    value -= 26
+                }
+                next += value
+            }
+        }
+        return next
+    }
+}
