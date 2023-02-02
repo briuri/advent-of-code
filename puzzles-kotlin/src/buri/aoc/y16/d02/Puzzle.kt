b3e555e62c8191af674792ec2af133fd4b0e2a31
buri <brian@urizone.net>
2023-02-02 11:55:02 -0500

    y16d02 solution.

---
+++ puzzles-kotlin/src/buri/aoc/y16/d02/Puzzle.kt
@@ -0,0 +1,86 @@
+package buri.aoc.y16.d02
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Grid
+import buri.aoc.common.Part
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun("1985", 1)
+        assertRun("74921", 0, true)
+    }
+    @Test
+    fun runPart2() {
+        assertRun("5DB3", 1)
+        assertRun("A6B35", 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): String {
+        val grid1 = Grid(3, 3)
+        grid1.set(0, 0, "1")
+        grid1.set(1, 0, "2")
+        grid1.set(2, 0, "3")
+        grid1.set(0, 1, "4")
+        grid1.set(1, 1, "5")
+        grid1.set(2, 1, "6")
+        grid1.set(0, 2, "7")
+        grid1.set(1, 2, "8")
+        grid1.set(2, 2, "9")
+
+        val grid2 = Grid(5, 5)
+        grid2.set(2, 0, "1")
+        grid2.set(1, 1, "2")
+        grid2.set(2, 1, "3")
+        grid2.set(3, 1, "4")
+        grid2.set(0, 2, "5")
+        grid2.set(1, 2, "6")
+        grid2.set(2, 2, "7")
+        grid2.set(3, 2, "8")
+        grid2.set(4, 2, "9")
+        grid2.set(1, 3, "A")
+        grid2.set(2, 3, "B")
+        grid2.set(3, 3, "C")
+        grid2.set(2, 4, "D")
+        val grid = if (part == Part.ONE) grid1 else grid2
+
+        var position = if (part == Part.ONE) Pair(1, 1) else Pair(0, 2)
+        var code = ""
+        for (line in input) {
+            for (value in line) {
+                val dx = when (value) {
+                    'L' -> -1
+                    'R' -> 1
+                    else -> 0
+                }
+                val dy = when (value) {
+                    'U' -> -1
+                    'D' -> 1
+                    else -> 0
+                }
+                val nextPosition = position.copy(first = position.first + dx, second = position.second + dy)
+                if (isInBounds(part, grid, nextPosition)) {
+                    position = nextPosition
+                }
+            }
+            code += grid.get(position)
+        }
+        return code
+    }
+
+    /**
+     * Checks if a point is in bounds, using different rules for Part 2.
+     */
+    private fun isInBounds(part: Part, grid: Grid, point: Pair<Int, Int>): Boolean {
+        return grid.isInBounds(point) && (part == Part.ONE || grid.get(point) != "0")
+    }
+}
