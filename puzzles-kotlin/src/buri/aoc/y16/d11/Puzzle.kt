b1b614b48829f9efb82c45fbf112e03efb4125db
buri <brian@urizone.net>
2023-02-05 20:19:07 -0500

    Code cleanup.

--- puzzles-kotlin/src/buri/aoc/y16/d11/Puzzle.kt
+++ puzzles-kotlin/src/buri/aoc/y16/d11/Puzzle.kt
@@ -25,7 +25,7 @@ class Puzzle : BasePuzzle() {
      * Executes a part of the puzzle using the specified input file.
      */
     override fun run(part: Part, input: List<String>): Number {
-        // Set up the start and end states.
+        // Set up the start and final states.
         val start = State(0, input[0])
         val builder = StringBuilder("4")
         for (i in 0 until start.totalPairs) {
@@ -33,6 +33,7 @@ class Puzzle : BasePuzzle() {
         }
         val end = State(0, builder.toString())
 
+        // Do a BFS to get to the final state.
         val frontier = mutableListOf<State>()
         val visited = mutableSetOf<String>()
         frontier.add(start)
@@ -46,7 +47,7 @@ class Puzzle : BasePuzzle() {
             for (next in current.getNextStates().filter { !visited.contains(it.toString()) }) {
                 frontier.add(next)
             }
-            frontier.sortByDescending { it.getScore() }
+            frontier.sortByDescending { it.getSortOrder() }
         }
         return -1
     }
@@ -57,11 +58,11 @@ class State(val steps: Int, private var state: String) {
     private val elevator = Character.getNumericValue(state[0])
 
     init {
-        // Sort pairs to reduce the number of unique states reached.
         val pairs = mutableListOf<String>()
         for (i in 0 until totalPairs) {
-            pairs.add(state.substring(toGeneratorIndex(i), toChipIndex(i) + 1))
+            pairs.add(state.substring(toIndex(i, true), toIndex(i, false) + 1))
         }
+        // Sort pairs to reduce the number of unique states reached.
         pairs.sort()
         val builder = StringBuilder()
         builder.append(state[0])
@@ -75,83 +76,69 @@ class State(val steps: Int, private var state: String) {
      * Determines what other states we can reach from where we are.
      */
     fun getNextStates(): List<State> {
-        val generatorsHere = getGeneratorsOnFloor(elevator)
-        val chipsHere = getChipsOnFloor(elevator)
-        val states = mutableListOf<State>()
+        val generatorsHere = getItemsOn(elevator, true)
+        val chipsHere = getItemsOn(elevator, false)
+        val nextStates = mutableListOf<State>()
 
         // Elevator can go up or down 1 floor.
-        val nextElevatorFloors = mutableListOf<Int>()
+        val nextFloors = mutableListOf<Int>()
         if (elevator <= 3) {
-            nextElevatorFloors.add(elevator + 1)
+            nextFloors.add(elevator + 1)
         }
         if (elevator >= 2) {
-            nextElevatorFloors.add(elevator - 1)
+            nextFloors.add(elevator - 1)
         }
-        for (nextFloor in nextElevatorFloors) {
+        for (nextFloor in nextFloors) {
             val nextFloorChar = nextFloor.digitToChar()
-            val generatorsNext = getGeneratorsOnFloor(nextFloor)
-            val chipsNext = getChipsOnFloor(nextFloor)
+            val nextGens = getItemsOn(nextFloor, true)
+            val nextChips = getItemsOn(nextFloor, false)
+            val nextSteps = steps + 1
 
             // Elevator can move matching pair (generator+chip) at any time.
-            for (generatorId in generatorsHere) {
-                if (chipsHere.contains(generatorId)) {
-                    val builder = StringBuilder(state)
-                    builder.setCharAt(0, nextFloorChar)
-                    builder.setCharAt(toGeneratorIndex(generatorId), nextFloorChar)
-                    builder.setCharAt(toChipIndex(generatorId), nextFloorChar)
-                    states.add(State(steps + 1, builder.toString()))
-                }
+            for (generatorId in generatorsHere.filter { chipsHere.contains(it) }) {
+                val builder = toBuilder(nextFloorChar)
+                builder.setCharAt(toIndex(generatorId, true), nextFloorChar)
+                builder.setCharAt(toIndex(generatorId, false), nextFloorChar)
+                nextStates.add(State(nextSteps, builder.toString()))
             }
 
             // Elevator can move 2 generators if they won't fry things on next floor.
-            for (generatorIds in getPairPermutations(generatorsHere)) {
-                if (areGeneratorsAllowedNear(generatorIds, generatorsNext, chipsNext)) {
-                    val builder = StringBuilder(state)
-                    builder.setCharAt(0, nextFloorChar)
-                    builder.setCharAt(toGeneratorIndex(generatorIds[0]), nextFloorChar)
-                    builder.setCharAt(toGeneratorIndex(generatorIds[1]), nextFloorChar)
-                    states.add(State(steps + 1, builder.toString()))
-                }
+            for (generatorIds in getPairPermutations(generatorsHere).filter { isGenAllowedNear(it, nextGens, nextChips) }) {
+                val builder = toBuilder(nextFloorChar)
+                builder.setCharAt(toIndex(generatorIds[0], true), nextFloorChar)
+                builder.setCharAt(toIndex(generatorIds[1], true), nextFloorChar)
+                nextStates.add(State(nextSteps, builder.toString()))
             }
 
             // Elevator can move 2 chips if they won't fry on next floor.
-            for (chipIds in getPairPermutations(chipsHere)) {
-                if (areChipsAllowedNear(chipIds, generatorsNext)) {
-                    val builder = StringBuilder(state)
-                    builder.setCharAt(0, nextFloorChar)
-                    builder.setCharAt(toChipIndex(chipIds[0]), nextFloorChar)
-                    builder.setCharAt(toChipIndex(chipIds[1]), nextFloorChar)
-                    states.add(State(steps + 1, builder.toString()))
-                }
+            for (chipIds in getPairPermutations(chipsHere).filter { isChipAllowedNear(it, nextGens) }) {
+                val builder = toBuilder(nextFloorChar)
+                builder.setCharAt(toIndex(chipIds[0], false), nextFloorChar)
+                builder.setCharAt(toIndex(chipIds[1], false), nextFloorChar)
+                nextStates.add(State(nextSteps, builder.toString()))
             }
 
             // Elevator can move 1 generator if it won't fry things on next floor.
-            for (generatorId in generatorsHere) {
-                if (areGeneratorsAllowedNear(mutableListOf(generatorId), generatorsNext, chipsNext)) {
-                    val builder = StringBuilder(state)
-                    builder.setCharAt(0, nextFloorChar)
-                    builder.setCharAt(toGeneratorIndex(generatorId), nextFloorChar)
-                    states.add(State(steps + 1, builder.toString()))
-                }
+            for (generatorId in generatorsHere.filter { isGenAllowedNear(mutableListOf(it), nextGens, nextChips) }) {
+                val builder = toBuilder(nextFloorChar)
+                builder.setCharAt(toIndex(generatorId, true), nextFloorChar)
+                nextStates.add(State(nextSteps, builder.toString()))
             }
 
             // Elevator can move 1 chip if it won't fry on next floor.
-            for (chipId in chipsHere) {
-                if (areChipsAllowedNear(mutableListOf(chipId), generatorsNext)) {
-                    val builder = StringBuilder(state)
-                    builder.setCharAt(0, nextFloorChar)
-                    builder.setCharAt(toChipIndex(chipId), nextFloorChar)
-                    states.add(State(steps + 1, builder.toString()))
-                }
+            for (chipId in chipsHere.filter { isChipAllowedNear(mutableListOf(it), nextGens) }) {
+                val builder = toBuilder(nextFloorChar)
+                builder.setCharAt(toIndex(chipId, false), nextFloorChar)
+                nextStates.add(State(nextSteps, builder.toString()))
             }
         }
-        return states
+        return nextStates
     }
 
     /**
      * Returns a score favoring items on higher floors.
      */
-    fun getScore(): Int {
+    fun getSortOrder(): Int {
         var score = 0
         for (i in 1 .. state.lastIndex) {
             if (state[i] in "1234") {
@@ -159,18 +146,6 @@ class State(val steps: Int, private var state: String) {
             }
         }
         return score
-    }
-
-    override fun toString(): String {
-        return state
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return (state == (other as State).state)
-    }
-
-    override fun hashCode(): Int {
-        return state.hashCode()
     }
 
     /**
@@ -192,38 +167,34 @@ class State(val steps: Int, private var state: String) {
     }
 
     /**
-     * Returns the IDs of all generators on some floor.
+     * Creates a string builder for modification
      */
-    private fun getGeneratorsOnFloor(floor: Int): List<Int> {
-        val generators = mutableListOf<Int>()
-        for (i in 0 until totalPairs) {
-            if (state[toGeneratorIndex(i)].digitToInt() == floor) {
-                generators.add(i)
-            }
-        }
-        return generators
+    private fun toBuilder(nextFloorChar: Char): StringBuilder {
+        val builder = StringBuilder(state)
+        builder.setCharAt(0, nextFloorChar)
+        return builder
     }
 
     /**
-     * Returns the IDs of all microchips on some floor.
+     * Returns the IDs of all generators on some floor.
      */
-    private fun getChipsOnFloor(floor: Int): List<Int> {
-        val chips = mutableListOf<Int>()
+    private fun getItemsOn(floor: Int, isGen: Boolean): List<Int> {
+        val items = mutableListOf<Int>()
         for (i in 0 until totalPairs) {
-            if (state[toChipIndex(i)].digitToInt() == floor) {
-                chips.add(i)
+            if (state[toIndex(i, isGen)].digitToInt() == floor) {
+                items.add(i)
             }
         }
-        return chips
+        return items
     }
 
     /**
      * Returns true if generators are allowed near chips.
      */
-    private fun areGeneratorsAllowedNear(generatorIds: List<Int>, generatorsNext: List<Int>, chipsNext: List<Int>): Boolean {
+    private fun isGenAllowedNear(generatorIds: List<Int>, nextGens: List<Int>, nextChips: List<Int>): Boolean {
         var allowed = true
-        for (chipId in chipsNext) {
-            allowed = allowed && (generatorIds.contains(chipId) || generatorsNext.contains(chipId))
+        for (chipId in nextChips) {
+            allowed = allowed && (generatorIds.contains(chipId) || nextGens.contains(chipId))
         }
         return allowed
     }
@@ -231,26 +202,32 @@ class State(val steps: Int, private var state: String) {
     /**
      * Returns true if chips are allowed near generators.
      */
-    private fun areChipsAllowedNear(chipIds: List<Int>, generatorsNext: List<Int>): Boolean {
+    private fun isChipAllowedNear(chipIds: List<Int>, nextGens: List<Int>): Boolean {
         var allowed = true
         for (id in chipIds) {
-            allowed = allowed && generatorsNext.contains(id)
+            allowed = allowed && nextGens.contains(id)
         }
-        allowed = allowed || generatorsNext.isEmpty()
+        allowed = allowed || nextGens.isEmpty()
         return allowed
     }
 
     /**
      * Converts a pair ID into its location in the state string.
      */
-    private fun toGeneratorIndex(id: Int): Int {
-        return 2 + id * 3
+    private fun toIndex(id: Int, isGen: Boolean): Int {
+        val offset = if (isGen) 2 else 3
+        return offset + id * 3
     }
 
-    /**
-     * Converts a pair ID into its location in the state string.
-     */
-    private fun toChipIndex(id: Int): Int {
-        return 3 + id * 3
+    override fun toString(): String {
+        return state
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return (state == (other as State).state)
+    }
+
+    override fun hashCode(): Int {
+        return state.hashCode()
     }
 }
