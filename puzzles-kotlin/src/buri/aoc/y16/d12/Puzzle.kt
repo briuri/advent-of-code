cb1bba04518d3c243c76fd792db9ff45edfa76a3
buri <brian@urizone.net>
2023-02-06 07:20:42 -0500

    y16d12 solution.

---
+++ puzzles-kotlin/src/buri/aoc/y16/d12/Puzzle.kt
@@ -0,0 +1,73 @@
+package buri.aoc.y16.d12
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Part
+import buri.aoc.common.Part.ONE
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun(42, 1)
+        assertRun(318020, 0, true)
+    }
+    @Test
+    fun runPart2() {
+        assertRun(9227674, 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): Number {
+        val registers = mutableMapOf<String, Long>()
+        registers["a"] = 0L
+        registers["b"] = 0L
+        registers["c"] = if (part == ONE) 0L else 1L
+        registers["d"] = 0L
+
+        var pointer = 0
+        while (pointer in input.indices) {
+            val command = input[pointer].split(" ")
+            if (command[0] == "cpy") {
+                registers[command[2]] = getValue(registers, command[1])
+                pointer++
+            }
+            else if (command[0] == "inc") {
+                registers[command[1]] = registers[command[1]]!! + 1
+                pointer++
+            }
+            else if (command[0] == "dec") {
+                registers[command[1]] = registers[command[1]]!! - 1
+                pointer++
+            }
+            else if (command[0] == "jnz") {
+                val offset = if (getValue(registers, command[1]) != 0L) {
+                    command[2].toInt()
+                }
+                else {
+                    1
+                }
+                pointer += offset
+            }
+        }
+        return registers["a"]!!
+    }
+
+    /**
+     * Converts an instruction token into a value from a register or a plain numeric value.
+     */
+    private fun getValue(registers: MutableMap<String, Long>, addressOrValue: String): Long {
+        return if (addressOrValue.toIntOrNull() == null) {
+            registers[addressOrValue]!!
+        }
+        else {
+            addressOrValue.toLong()
+        }
+    }
+}
