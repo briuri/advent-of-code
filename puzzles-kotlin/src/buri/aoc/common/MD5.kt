7d576636936b0e1df6f5ec3bc19a650cd062fc11
buri <brian@urizone.net>
2023-02-06 19:39:20 -0500

    y16d14 solution.

--- puzzles-kotlin/src/buri/aoc/common/MD5.kt
+++ puzzles-kotlin/src/buri/aoc/common/MD5.kt
@@ -11,9 +11,24 @@ class MD5 {
     private val digest = MessageDigest.getInstance("MD5")
 
     /**
+     * Creates an MD5 hash of some string. Rehashes multiple times before converting to a lowercase
+     * hex string.
+     * (y16d14)
+     */
+    fun getMultipleHashes(input: String, extraHashes: Int = 0): String {
+        var hash = digest.digest(input.toByteArray())
+        for (i in 0 until extraHashes) {
+           hash = reHash(hash)
+        }
+        return toHex(hash)
+    }
+
+    /**
      * Creates an MD5 hash of some string. Tries to save time by not doing the hex conversion when
      * the leading zeroes aren't present. Returns empty string if the hash doesn't have the right
      * number of leading zeroes.
+     *
+     * (y15d04, y16d05)
      */
     fun getHashWithLeadingZeroes(input: String, leadingZeroes: Int = 0): String {
         val bytes = digest.digest(input.toByteArray())
@@ -31,6 +46,21 @@ class MD5 {
             }
         }
         return toHex(bytes)
+    }
+
+    /**
+     * Rehashes a hash.
+     */
+    private fun reHash(bytes: ByteArray): ByteArray {
+        val output = ByteArray(bytes.size * 2)
+        var j = 0
+        for (b in bytes) {
+            output[j] = Character.forDigit(b.toInt() shr 4 and 0xF, 16).code.toByte()
+            output[j + 1] = Character.forDigit(b.toInt() and 0xF, 16).code.toByte()
+            j += 2
+        }
+        digest.update(output)
+        return digest.digest()
     }
 
     /**
---
+++ puzzles-kotlin/src/buri/aoc/y16/d14/Puzzle.kt
@@ -0,0 +1,61 @@
+package buri.aoc.y16.d14
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.MD5
+import buri.aoc.common.Part
+import buri.aoc.common.Part.ONE
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun(22728, 1)
+        assertRun(15168, 0, true)
+    }
+    @Test
+    fun runPart2() {
+        assertRun(22551, 1)
+        assertRun(20864, 0, true)
+    }
+
+    private val tripleRegEx = "([a-f0-9])\\1{2}".toRegex()
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): Number {
+        val md5 = MD5()
+        val salt = input[0]
+        val stretches = if (part == ONE) 0 else 2016
+
+        val hashes = mutableMapOf<Int, String>()
+        var index = 0
+        var keys = 0
+        while (keys != 64) {
+            if (hashes[index] == null) {
+                hashes[index] = md5.getMultipleHashes(salt + index, stretches)
+            }
+            val firstTriple = tripleRegEx.find(hashes[index]!!)
+            if (firstTriple != null) {
+                val repeating = firstTriple.value[0]
+                for (quintIndex in index + 1 until index + 1000) {
+                    if (hashes[quintIndex] == null) {
+                        hashes[quintIndex] = md5.getMultipleHashes(salt + quintIndex, stretches)
+                    }
+                    val quintRegEx = "($repeating)\\1{4}".toRegex()
+                    if (quintRegEx.containsMatchIn(hashes[quintIndex]!!)) {
+                        keys++
+                        break
+                    }
+                }
+            }
+            index++
+        }
+        return index - 1
+    }
+}
