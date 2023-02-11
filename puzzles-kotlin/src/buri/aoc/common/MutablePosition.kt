07c6824d1798856c873c5e20ad0a03f3d09ee665
buri <brian@urizone.net>
2023-02-11 06:05:34 -0500

    Moved MutablePosition and Direction to common so it can be reused on y17d03.

---
+++ puzzles-kotlin/src/buri/aoc/common/MutablePosition.kt
@@ -0,0 +1,53 @@
+package buri.aoc.common
+
+import buri.aoc.common.Direction.NORTH
+import buri.aoc.common.Direction.EAST
+import buri.aoc.common.Direction.SOUTH
+import buri.aoc.common.Direction.WEST
+import kotlin.math.absoluteValue
+
+enum class Direction { NORTH, EAST, SOUTH, WEST }
+data class MutablePosition(var coords: Pair<Int, Int>, var facing: Direction) {
+
+    /**
+     * Move one square in the current direction.
+     */
+    fun move() {
+        coords = when (facing) {
+            NORTH -> coords.copy(second = coords.second + 1)
+            EAST -> coords.copy(first = coords.first + 1)
+            SOUTH -> coords.copy(second = coords.second - 1)
+            WEST -> coords.copy(first = coords.first - 1)
+        }
+    }
+
+    /**
+     * Change the facing on this position
+     */
+    fun turn(turn: Char) {
+        if (turn == 'R') {
+            facing = when (facing) {
+                NORTH -> EAST
+                EAST -> SOUTH
+                SOUTH -> WEST
+                WEST -> NORTH
+            }
+        }
+        // L
+        else {
+            facing = when (facing) {
+                NORTH -> WEST
+                EAST -> NORTH
+                SOUTH -> EAST
+                WEST -> SOUTH
+            }
+        }
+    }
+
+    /**
+     * Get the Manhattan distance of this position from the origin.
+     */
+    fun getManhattanDistance(): Int {
+        return coords.first.absoluteValue + coords.second.absoluteValue
+    }
+}
--- puzzles-kotlin/src/buri/aoc/y16/d01/Puzzle.kt
+++ puzzles-kotlin/src/buri/aoc/y16/d01/Puzzle.kt
@@ -1,11 +1,11 @@
 package buri.aoc.y16.d01
 
 import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Direction
+import buri.aoc.common.MutablePosition
 import buri.aoc.common.Part
 import buri.aoc.common.Part.TWO
-import buri.aoc.y16.d01.Direction.*
 import org.junit.Test
-import kotlin.math.absoluteValue
 
 /**
  * Entry point for a daily puzzle
@@ -29,7 +29,7 @@ class Puzzle : BasePuzzle() {
      */
     override fun run(part: Part, input: List<String>): Number {
         val visited = mutableSetOf<Pair<Int, Int>>()
-        val position = MutablePosition(Pair(0, 0), NORTH)
+        val position = MutablePosition(Pair(0, 0), Direction.NORTH)
         for (command in input[0].split(", ")) {
             position.turn(command[0])
             for (i in 0 until command.drop(1).toInt()) {
@@ -41,50 +41,5 @@ class Puzzle : BasePuzzle() {
             }
         }
         return position.getManhattanDistance()
-    }
-}
-enum class Direction { NORTH, EAST, SOUTH, WEST }
-data class MutablePosition(var coords: Pair<Int, Int>, var facing: Direction) {
-
-    /**
-     * Move one square in the current direction.
-     */
-    fun move() {
-        coords = when (facing) {
-            NORTH -> coords.copy(second = coords.second + 1)
-            EAST -> coords.copy(first = coords.first + 1)
-            SOUTH -> coords.copy(second = coords.second - 1)
-            WEST -> coords.copy(first = coords.first - 1)
-        }
-    }
-
-    /**
-     * Change the facing on this position
-     */
-    fun turn(turn: Char) {
-        if (turn == 'R') {
-            facing = when (facing) {
-                NORTH -> EAST
-                EAST -> SOUTH
-                SOUTH -> WEST
-                WEST -> NORTH
-            }
-        }
-        // L
-        else {
-            facing = when (facing) {
-                NORTH -> WEST
-                EAST -> NORTH
-                SOUTH -> EAST
-                WEST -> SOUTH
-            }
-        }
-    }
-
-    /**
-     * Get the Manhattan distance of this position from the origin.
-     */
-    fun getManhattanDistance(): Int {
-        return coords.first.absoluteValue + coords.second.absoluteValue
     }
 }
