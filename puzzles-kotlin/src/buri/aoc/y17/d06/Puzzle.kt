5153224612c4effe14fb224337d5198220a7982e
buri <brian@urizone.net>
2023-02-11 14:06:38 -0500

    y17d06 solution.

---
+++ puzzles-kotlin/src/buri/aoc/y17/d06/Puzzle.kt
@@ -0,0 +1,65 @@
+package buri.aoc.y17.d06
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Part
+import buri.aoc.common.Part.TWO
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun(5, 1)
+        assertRun(7864, 0, true)
+    }
+    @Test
+    fun runPart2() {
+        assertRun(4, 1)
+        assertRun(1695, 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): Number {
+        val banks = mutableListOf<Int>()
+        for (blocks in collapseWhitespace(input[0]).split(" ")) {
+            banks.add(blocks.toInt())
+        }
+
+        val visited = mutableSetOf<String>()
+        var steps = 0
+        var findingCycle = false
+        while (true) {
+            visited.add(banks.toString())
+            var target = banks.indexOfFirst { it == banks.max() }
+            var blocks = banks[target]
+            banks[target] = 0
+            while (blocks > 0) {
+                target++
+                if (target > banks.lastIndex) {
+                    target = 0
+                }
+                banks[target] += 1
+                blocks--
+            }
+            steps++
+
+            // In part two, reset the states and find the next cycle.
+            if (visited.contains(banks.toString())) {
+                if (part == TWO && !findingCycle) {
+                    steps = 0
+                    visited.clear()
+                    findingCycle = true
+                }
+                else {
+                    return steps
+                }
+            }
+        }
+    }
+}
