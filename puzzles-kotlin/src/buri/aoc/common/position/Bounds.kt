861f35f246b03f5c6bb29fd4631f5bcde5652b52
buri <brian@urizone.net>
2023-02-21 15:00:17 -0500

    Added bounds utility class.

---
+++ puzzles-kotlin/src/buri/aoc/common/position/Bounds.kt
@@ -0,0 +1,26 @@
+package buri.aoc.common.position
+
+/**
+ * The rectangular bounds for a set of points.
+ *
+ * @author Brian Uri!
+ */
+data class Bounds(val points: Set<Pair<Int, Int>>) {
+    val x: IntRange
+    val y: IntRange
+
+    init {
+        var minX = Int.MAX_VALUE
+        var maxX = Int.MIN_VALUE
+        var minY = Int.MAX_VALUE
+        var maxY = Int.MIN_VALUE
+        for (point in points) {
+            minX = minX.coerceAtMost(point.first)
+            maxX = maxX.coerceAtLeast(point.first)
+            minY = minY.coerceAtMost(point.second)
+            maxY = maxY.coerceAtLeast(point.second)
+        }
+        x = minX..maxX
+        y = minY..maxY
+    }
+}
--- puzzles-kotlin/src/buri/aoc/y18/d06/Puzzle.kt
+++ puzzles-kotlin/src/buri/aoc/y18/d06/Puzzle.kt
@@ -5,6 +5,7 @@ import buri.aoc.common.Part
 import buri.aoc.common.Part.ONE
 import buri.aoc.common.extractInts
 import buri.aoc.common.getManhattanDistance
+import buri.aoc.common.position.Bounds
 import org.junit.Test
 
 /**
@@ -29,24 +30,16 @@ class Puzzle : BasePuzzle() {
      */
     override fun run(part: Part, input: List<String>): Number {
         val points = mutableSetOf<Pair<Int, Int>>()
-        var minX = Int.MAX_VALUE
-        var maxX = Int.MIN_VALUE
-        var minY = Int.MAX_VALUE
-        var maxY = Int.MIN_VALUE
         for (line in input) {
             val numbers = line.extractInts()
-            // Calculate the rectangular bounds of all points
-            minX = minX.coerceAtMost(numbers[0])
-            maxX = maxX.coerceAtLeast(numbers[0])
-            minY = minY.coerceAtMost(numbers[1])
-            maxY = maxY.coerceAtLeast(numbers[1])
             points.add(Pair(numbers[0], numbers[1]))
         }
+        val bounds = Bounds(points)
 
         val regionSizes = mutableMapOf<Pair<Int, Int>, Int>()
         val distanceSums = mutableMapOf<Pair<Int, Int>, Int>()
-        for (y in minY..maxY) {
-            for (x in minX..maxX) {
+        for (y in bounds.y) {
+            for (x in bounds.x) {
                 if (part == ONE) {
                     val closest = getClosest(points, Pair(x, y))
                     if (closest != null) {
