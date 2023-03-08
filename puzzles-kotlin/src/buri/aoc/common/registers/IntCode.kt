e4241d03873ef07f74f9d35dd9f435f4f1a33e61
buri <brian@urizone.net>
2023-03-07 20:20:54 -0500

    Added debugging output to IntCode computer.

--- puzzles-kotlin/src/buri/aoc/common/registers/IntCode.kt
+++ puzzles-kotlin/src/buri/aoc/common/registers/IntCode.kt
@@ -11,22 +11,34 @@ class Computer(private val instructions: List<Int>) {
     /**
      * Runs the IntCode
      */
-    fun run(noun: Int, verb: Int): Int {
+    fun run(noun: Int, verb: Int, debug: Boolean = false): Int {
+        if (debug) {
+            println("Computer running with [1]=$noun [2]=$verb")
+        }
         val code = instructions.toMutableList()
         code[1] = noun
         code[2] = verb
 
-        var pointer = 0
-        while (code[pointer] != 99) {
-            val in1 = code[pointer + 1]
-            val in2 = code[pointer + 2]
-            val out = code[pointer + 3]
-            code[out] = when (code[pointer]) {
+        var ip = 0
+        while (code[ip] != 99) {
+            val in1 = code[ip + 1]
+            val in2 = code[ip + 2]
+            val out = code[ip + 3]
+            if (debug) {
+                print("ip=$ip (${code[ip]},$in1,$in2,$out) | ")
+                when (code[ip]) {
+                    1 -> print("[$out] = [$in1] + [$in2] ► ${code[in1]} + ${code[in2]} ► ${code[in1] + code[in2]}")
+                    2 -> print("[$out] = [$in1] * [$in2] ► ${code[in1]} * ${code[in2]} ► ${code[in1] * code[in2]}")
+                    else -> continue
+                }
+                println()
+            }
+            code[out] = when (code[ip]) {
                 1 -> code[in1] + code[in2]
                 2 -> code[in1] * code[in2]
                 else -> continue
             }
-            pointer += 4
+            ip += 4
         }
         return code[0]
     }
