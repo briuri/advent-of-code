093806c3b960ffc06288064593789626f6a3ae0c
buri <brian@urizone.net>
2023-03-09 08:24:34 -0500

    y19d05 solution.

--- puzzles-kotlin/src/buri/aoc/common/registers/IntCode.kt
+++ puzzles-kotlin/src/buri/aoc/common/registers/IntCode.kt
@@ -2,44 +2,146 @@ package buri.aoc.common.registers
 
 /**
  * Computer for IntCode problems
- * (y19d02)
+ * (y19d02,y19d05)
  *
  * @author Brian Uri!
  */
-class Computer(private val instructions: List<Int>) {
+class Computer(private val instructions: List<Int>, private val debug: Boolean = false) {
+    private val inputs = mutableListOf<Int>()
+    val outputs = mutableListOf<Int>()
 
     /**
      * Runs the IntCode
      */
-    fun run(noun: Int, verb: Int, debug: Boolean = false): Int {
-        if (debug) {
-            println("Computer running with [1]=$noun [2]=$verb")
-        }
+    fun run(noun: Int? = null, verb: Int? = null, rawInputs: List<Int> = listOf()): Int {
+        inputs.clear()
+        inputs.addAll(rawInputs)
+        outputs.clear()
         val code = instructions.toMutableList()
-        code[1] = noun
-        code[2] = verb
+        if (noun != null) {
+            code[1] = noun
+        }
+        if (verb != null) {
+            code[2] = verb
+        }
 
         var ip = 0
         while (code[ip] != 99) {
-            val in1 = code[ip + 1]
-            val in2 = code[ip + 2]
-            val out = code[ip + 3]
-            if (debug) {
-                print("ip=$ip (${code[ip]},$in1,$in2,$out) | ")
-                when (code[ip]) {
-                    1 -> print("[$out] = [$in1] + [$in2] ► ${code[in1]} + ${code[in2]} ► ${code[in1] + code[in2]}")
-                    2 -> print("[$out] = [$in1] * [$in2] ► ${code[in1]} * ${code[in2]} ► ${code[in1] * code[in2]}")
-                    else -> continue
+            val c = Command(code[ip])
+            val p = code.subList(ip + 1, ip + 1 + c.numParams)
+            debug(code, ip, c, p)
+            ip += c.numParams + 1
+            when (c.opcode) {
+                ADD -> code[p[2]] = code.resolve(p[0], c.mode0) + code.resolve(p[1], c.mode1)
+                MUL -> code[p[2]] = code.resolve(p[0], c.mode0) * code.resolve(p[1], c.mode1)
+                IN -> code[p[0]] = inputs.removeFirst()
+                OUT -> outputs.add(code.resolve(p[0], c.mode0))
+                JIT -> if (code.resolve(p[0], c.mode0) != 0) {
+                    ip = code.resolve(p[1], c.mode1)
                 }
-                println()
+                JIF -> if (code.resolve(p[0], c.mode0) == 0) {
+                    ip = code.resolve(p[1], c.mode1)
+                }
+                LT -> code[p[2]] = if (code.resolve(p[0], c.mode0) < code.resolve(p[1], c.mode1)) 1 else 0
+                EQ -> code[p[2]] = if (code.resolve(p[0], c.mode0) == code.resolve(p[1], c.mode1)) 1 else 0
             }
-            code[out] = when (code[ip]) {
-                1 -> code[in1] + code[in2]
-                2 -> code[in1] * code[in2]
-                else -> continue
-            }
-            ip += 4
         }
         return code[0]
     }
+
+    /**
+     * Prints some output if debug mode is enabled.
+     */
+    private fun debug(code: MutableList<Int>, ip: Int, c: Command, p: List<Int>) {
+        if (debug) {
+            print("ip=$ip c=(${code[ip]},${p.joinToString(",")}) ► ")
+            val p0 = code.debugResolve(p[0], c.mode0)
+            val p0Resolved = code.debugResolve(p[0], c.mode0, true)
+            val p1 = if (c.numParams > 1) {
+                code.debugResolve(p[1], c.mode1)
+            } else {
+                "?"
+            }
+            val p1Resolved = if (c.numParams > 1) {
+                code.debugResolve(p[1], c.mode1, true)
+            } else {
+                "?"
+            }
+            val p2 = if (c.numParams > 2) {
+                code.debugResolve(p[2], c.mode2)
+            } else {
+                "?"
+            }
+            when (c.opcode) {
+                ADD, MUL -> {
+                    val op = if (c.opcode == ADD) "+" else "*"
+                    print("$p2 = $p0 $op $p1 ► $p0Resolved $op $p1Resolved")
+                }
+                IN -> {
+                    print("$p0 = ${inputs[0]}")
+                }
+                OUT -> {
+                    print("out = $p0 ► out = $p0Resolved")
+                }
+                JIT, JIF -> {
+                    val op = if (c.opcode == JIT) "!=" else "=="
+                    print("if ($p0 $op 0) ip = $p1 ► if ($p0Resolved $op 0) ip = $p1Resolved")
+                }
+                LT, EQ -> {
+                    val op = if (c.opcode == LT) "<" else "=="
+                    print("$p2 = if ($p0 $op $p1) 1 else 0 ► if ($p0Resolved $op $p1Resolved) 1 else 0")
+                }
+            }
+            println()
+        }
+    }
+
+    /**
+     * Returns a value or the value at that address, depending on the mode.
+     */
+    private fun MutableList<Int>.resolve(param: Int, mode: Int): Int = if (mode == 0) this[param] else param
+
+    /**
+     * Creates debug output for an address or value, depending on the mode.
+     */
+    private fun MutableList<Int>.debugResolve(param: Int, mode: Int, resolveAddress: Boolean = false): String {
+        return if (mode == 0) {
+            if (resolveAddress) {
+                "${this[param]}"
+            } else {
+                "[$param]"
+            }
+        } else {
+            "$param"
+        }
+    }
 }
+
+class Command(rawCommand: Int) {
+    val opcode = rawCommand % 100
+    val mode0: Int
+    val mode1: Int
+    val mode2: Int
+    val numParams: Int
+        get() = when (opcode) {
+            IN, OUT -> 1
+            JIT, JIF -> 2
+            else -> 3
+        }
+
+    init {
+        val fullCommand = rawCommand.toString().padStart(5, '0')
+        mode0 = fullCommand[2].digitToInt()
+        mode1 = fullCommand[1].digitToInt()
+        mode2 = fullCommand[0].digitToInt()
+    }
+}
+
+const val ADD = 1
+const val MUL = 2
+const val IN = 3
+const val OUT = 4
+const val JIT = 5
+const val JIF = 6
+const val LT = 7
+const val EQ = 8
---
+++ puzzles-kotlin/src/buri/aoc/y19/d05/Puzzle.kt
@@ -0,0 +1,34 @@
+package buri.aoc.y19.d05
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Part
+import buri.aoc.common.extractInts
+import buri.aoc.common.registers.Computer
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun(6069343, 0, true)
+    }
+
+    @Test
+    fun runPart2() {
+        assertRun(3188550, 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): Number {
+        val computer = Computer(input[0].extractInts())
+        val startValue = if (part.isOne()) 1 else 5
+        computer.run(rawInputs = listOf(startValue))
+        return computer.outputs.last()
+    }
+}
