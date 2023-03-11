014adca6f1c11d3bac7b328f5e670b3833904bfa
buri <brian@urizone.net>
2023-03-10 19:51:27 -0500

    y19d11 solution.

---
+++ puzzles-kotlin/src/buri/aoc/y19/d11/Puzzle.kt
@@ -0,0 +1,62 @@
+package buri.aoc.y19.d11
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Part
+import buri.aoc.common.extractInts
+import buri.aoc.common.extractLongs
+import buri.aoc.common.position.*
+import buri.aoc.common.registers.Computer
+import org.junit.Test
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun("2276", 0, true)
+    }
+
+    @Test
+    fun runPart2() {
+        // CBLPJZCU
+        assertRun("  ■■  ■■■  ■    ■■■    ■■ ■■■■  ■■  ■  ■", 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): String {
+        val position = MutablePosition(Point2D(0, 0), Direction.NORTH)
+        val painted = mutableMapOf<Point2D<Int>, Int>()
+        if (part.isTwo()) {
+            painted[position.coords] = 1
+        }
+
+        val computer = Computer(input[0].extractLongs())
+        while (!computer.halted) {
+            val color = painted.getOrDefault(position.coords, 0)
+            computer.inputs.add(color.toLong())
+            computer.run()
+            painted[position.coords] = computer.outputs.removeFirst().toInt()
+            if (computer.outputs.removeFirst().toInt() == 0) {
+                position.turnLeft()
+            } else {
+                position.turnRight()
+            }
+            position.move()
+        }
+
+        if (part.isOne()) {
+            return painted.size.toString()
+        }
+
+        val grid = Grid(50, 6, ' ')
+        for ((point, _) in painted.filter { it.value == 1 }) {
+            grid[point] = '■'
+        }
+        return grid.toString()
+    }
+}
