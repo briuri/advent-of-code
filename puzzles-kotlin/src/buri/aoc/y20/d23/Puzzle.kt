5eada75a1a99c9bf458ca2c33a6ad65828a68ec1
buri <brian@urizone.net>
2023-03-27 14:06:10 -0400

    Refactored to use a List instead of a Map, cutting Part 2 time in half.

--- puzzles-kotlin/src/buri/aoc/y20/d23/Puzzle.kt
+++ puzzles-kotlin/src/buri/aoc/y20/d23/Puzzle.kt
@@ -18,28 +18,39 @@ class Puzzle : BasePuzzle() {
 
     @Test
     fun runPart2() {
-        // Skipping example to reduce test suite time.
-//        assertRun("149245887792", 1)
+        assertRun("149245887792", 1)
         assertRun("519044017360", 0, true)
     }
+
+    // Cup 1 is at index 0 in the list of cups.
+    private val indexOffset = 1
 
     /**
      * Executes a part of the puzzle using the specified input file.
      */
     override fun run(part: Part, input: List<String>): String {
-        val nextCups = mutableMapOf<Int, Int>()
-        val cups = input[0].map { it.digitToInt() }.toMutableList()
+        val rawCups = input[0].map { it.digitToInt() }
+        val totalCups = if (part.isOne()) rawCups.size else 1_000_000
+
+        // 0-based not 1-baesd
+        val nextCups = MutableList(totalCups) { 0 }
+        for (index in rawCups.indices) {
+            val nextCup = if (part.isOne() && index == rawCups.lastIndex) {
+                rawCups[0]
+            } else if (part.isTwo() && index == rawCups.lastIndex) {
+                rawCups.size + 1
+            } else {
+                rawCups[index + 1]
+            }
+            nextCups[rawCups[index] - indexOffset] = nextCup
+        }
         if (part.isTwo()) {
-            for (i in cups.size + 1 .. 1_000_000) {
-                cups.add(i)
+            for (cup in rawCups.size + 1 .. totalCups) {
+                nextCups[cup - indexOffset] = if (cup == totalCups) rawCups[0] else (cup + 1)
             }
         }
-        for (i in cups.indices) {
-            val cup = cups[i]
-            val nextIndex = if (i == cups.lastIndex) 0 else i + 1
-            nextCups[cup] = cups[nextIndex]
-        }
-        var current = cups[0]
+
+        var current = rawCups[0]
         val times = if (part.isOne()) 100 else 10_000_000
         repeat (times) {
             current = move(current, nextCups)
@@ -50,13 +61,13 @@ class Puzzle : BasePuzzle() {
             current = 1
             while (current.toString() !in result) {
                 result.append(current)
-                current = nextCups[current]!!
+                current = nextCups[current - indexOffset]
             }
             return result.toString().drop(1)
         }
 
-        val first = nextCups[1]!!
-        val second = nextCups[first]!!.toLong()
+        val first = nextCups[1 - indexOffset]
+        val second = nextCups[first - indexOffset].toLong()
         return (first * second).toString()
 
     }
@@ -64,30 +75,22 @@ class Puzzle : BasePuzzle() {
     /**
      * Does 1 turn in the game and returns the current cup.
      */
-    private fun move(cup0: Int, nextCups: MutableMap<Int, Int>): Int {
-        val cup1 = nextCups[cup0]!!
-        val cup2 = nextCups[cup1]!!
-        val cup3 = nextCups[cup2]!!
+    private fun move(cup0: Int, nextCups: MutableList<Int>): Int {
+        val cup1 = nextCups[cup0 - indexOffset]
+        val cup2 = nextCups[cup1 - indexOffset]
+        val cup3 = nextCups[cup2 - indexOffset]
         val minCup = 1
         val maxCup = nextCups.size
 
-        var destination = cup0 - 1
-        if (destination < minCup) {
-            destination = maxCup
-        }
+        var destination = if (cup0 == 1) maxCup else (cup0 - 1)
         while (destination == cup1 || destination == cup2 || destination == cup3) {
-            destination -= 1
-            if (destination < minCup) {
-                destination = maxCup
-            }
+            destination = if (destination == minCup) maxCup else (destination - 1)
         }
 
-        // Extract sublist.
-        nextCups[cup0] = nextCups[cup3]!!
-        // Reinsert sublist.
-        nextCups[cup3] = nextCups[destination]!!
-        nextCups[destination] = cup1
-
-        return nextCups[cup0]!!
+        // Extract and reinsert sublist.
+        nextCups[cup0 - indexOffset] = nextCups[cup3 - indexOffset]
+        nextCups[cup3 - indexOffset] = nextCups[destination - indexOffset]
+        nextCups[destination - indexOffset] = cup1
+        return nextCups[cup0 - indexOffset]
     }
 }
