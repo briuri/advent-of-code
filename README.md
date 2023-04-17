2987c138507a420b3dafe9bdb788f535fc16bc2c
buri <brian@urizone.net>
2023-04-17 11:59:13 -0400

    y22d25 solution.

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 This project contains my Advent of Code solutions. 
 
-I've solved every puzzle from 2015-2022 in Java (2018-2022 were done in real-time during the midnight competitions).
-I'm currently going back through and solving older puzzles again as a way to learn Kotlin.
+I've solved every puzzle from 2015-2022 in Java and Kotlin (2018-2022 were done in real-time during the midnight competitions).
 
 The top-level of the project can be imported as an IntelliJ IDEA project. Solutions are found under 
 `puzzles-<language>/src/` with a package naming convention of `buri.aoc.y<yy>.d<dd>`. Each Day can be run from a JUnit 
@@ -12,17 +11,17 @@ under `/src/test/` when competing).
 
 ## Test Suite Run Time
 
-| Year      | Java  | Kotlin |
-|-----------|-------|--------|
-| **2015**  | 00:30 | 00:10  |
-| **2016**  | 00:45 | 00:35  |
-| **2017**  | 00:30 | 00:15  |
-| **2018**  | 00:35 | 00:20  |
-| **2019**  | 00:15 | 00:10  |
-| **2020**  | 00:25 | 00:20  |
-| **2021**  | 01:40 | 00:50  |
-| **2022**  | 00:50 |        |
-| **Total** | 05:30 | 02:40  |
+| Year      | Java      | Kotlin    |
+|-----------|-----------|-----------|
+| **2015**  | 00:30     | 00:10     |
+| **2016**  | 00:45     | 00:35     |
+| **2017**  | 00:30     | 00:15     |
+| **2018**  | 00:35     | 00:20     |
+| **2019**  | 00:15     | 00:10     |
+| **2020**  | 00:25     | 00:20     |
+| **2021**  | 01:40     | 00:50     |
+| **2022**  | 00:50     | 01:00     |
+| **Total** | **05:30** | **03:40** |
 
 ## Leaderboard Visualization
 
---
+++ puzzles-kotlin/src/buri/aoc/y22/d25/Puzzle.kt
@@ -0,0 +1,74 @@
+package buri.aoc.y22.d25
+
+import buri.aoc.common.BasePuzzle
+import buri.aoc.common.Part
+import org.junit.Test
+import kotlin.math.pow
+
+/**
+ * Entry point for a daily puzzle
+ *
+ * @author Brian Uri!
+ */
+class Puzzle : BasePuzzle() {
+    @Test
+    fun runPart1() {
+        assertRun("2=-1=0", 1)
+        assertRun("2=-0=1-0012-=-2=0=01", 0, true)
+    }
+
+    /**
+     * Executes a part of the puzzle using the specified input file.
+     */
+    override fun run(part: Part, input: List<String>): String {
+        return toSnafu(input.sumOf { toLong(it) })
+    }
+
+    /**
+     * Converts from snafu to long.
+     */
+    private fun toLong(snafu: String): Long {
+        var total = 0L
+        for (i in snafu.lastIndex downTo 0) {
+            val place = 5.toDouble().pow(snafu.lastIndex - i).toLong()
+            when (snafu[i]) {
+                '=' -> total -= place * 2
+                '-' -> total -= place
+                '1' -> total += place
+                '2' -> total += place * 2
+            }
+        }
+        return total
+    }
+
+    /**
+     * Converts from long to snafu.
+     */
+    private fun toSnafu(value: Long): String {
+        var remaining = value
+        val output = StringBuilder()
+        while (remaining != 0L) {
+            when (remaining % 5) {
+                0L -> output.append("0")
+                1L -> {
+                    output.append("1")
+                    remaining -= 1
+                }
+                2L -> {
+                    output.append("2")
+                    remaining -= 2
+                }
+                3L -> {
+                    output.append("=")
+                    remaining += 2
+                }
+                4L -> {
+                    output.append("-")
+                    remaining += 1
+                }
+            }
+            remaining /= 5
+        }
+        return output.reverse().toString()
+    }
+}
