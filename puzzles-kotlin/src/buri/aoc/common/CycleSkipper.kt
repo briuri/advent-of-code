0c8c8df68a5e445f0ccd030fc46989f66b720982
buri <brian@urizone.net>
2023-12-15 20:06:46 -0500

    Minor refactoring.

--- puzzles-kotlin/src/buri/aoc/common/CycleSkipper.kt
+++ puzzles-kotlin/src/buri/aoc/common/CycleSkipper.kt
@@ -13,25 +13,27 @@ package buri.aoc.common
 class CycleSkipper(private val keyStrategy: () -> String, private val stepStrategy: () -> Unit) {
 
     /**
-     * Starts at step 0 and calculates the first time a cycle occurs, its length, and (given a target end step)
+     * Starts a process and calculates the first time a cycle occurs, its length, and (given a target end step)
      * the last time the cycle will occur before the process ends. Then jumps ahead to the last cycle
      * and finishes all iterations.
+     *
+     * The initial state is stored as step 0, so step 1 is the first iteration through the process.
      */
     fun run(targetEnd: Int) {
         val states = mutableMapOf<String, Int>()
         var step = 0
         while (step < targetEnd) {
-            val stateKey = keyStrategy.invoke()
-            if (stateKey in states.keys) {
-                val firstCycle = states[stateKey]!!
+            val stateKey = keyStrategy()
+            val firstCycle = states[stateKey]
+            if (firstCycle != null) {
                 val cycleLength = step - firstCycle
                 val remainingCycles = (targetEnd - step) / cycleLength + 1  // integer division
-                // Skip ahead a whole number of cycles (integer division, so floor value).
+                // Skip ahead a whole number of cycles
                 step = firstCycle + cycleLength * remainingCycles
             }
             states[stateKey] = step
             step++
-            stepStrategy.invoke()
+            stepStrategy()
         }
     }
 }
