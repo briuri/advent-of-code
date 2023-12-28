4bfd99f78e85d04e46a563f25d7f63e8db4135f8
buri <brian@urizone.net>
2023-12-28 13:22:01 -0500

    Added utility to count LOC (non-blank) in each year's solutions.

---
+++ viz-kotlin/src/LineCounter.kt
@@ -0,0 +1,40 @@
+import java.io.File
+
+/**
+ * Simple utility to count lines of code in competition files. Ignores whitespace lines.
+ *
+ * @author Brian Uri!
+ */
+fun main() {
+    val pathPrefix = "C:\\workspace\\advent-of-code"
+    val javaPrefix = "$pathPrefix\\puzzles-java\\src\\buri\\aoc"
+    val kotlinPrefix = "$pathPrefix\\puzzles-kotlin\\src\\buri\\aoc"
+
+    println("Java Solutions:")
+    for (year in 15..22) {
+        val loc = countLines("$javaPrefix\\y$year", year)
+        println("20$year\t$loc")
+    }
+    println("Kotlin Solutions:")
+    for (year in 15..23) {
+        val loc = countLines("$kotlinPrefix\\y$year", year)
+        println("20$year\t$loc")
+    }
+}
+
+/**
+ * Counts the lines of code in a particular path for the given years
+ */
+fun countLines(prefix: String, year: Int): Int {
+    var loc = 0
+    for (day in 1..25) {
+        val paddedDay = if (day < 10) "0$day" else "$day"
+        val dirPath = "$prefix\\d$paddedDay"
+        for (file in File("$dirPath").listFiles()) {
+            val lines = file.readLines()
+            val count = lines.filter { it != "" }.size
+            loc += count
+        }
+    }
+    return loc
+}
